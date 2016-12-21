6e4cce69a88d41b003df83fe6e40ac87e570eb65
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2016-12-21 18:56:38 +0200

    Separate a copyright header from the JavaDoc section in `package-info`s across the project.

--- plugin/src/main/java/org/spine3/gradle/reflections/package-info.java
+++ plugin/src/main/java/org/spine3/gradle/reflections/package-info.java
@@ -16,8 +16,9 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *
+ */
+
+/**
  * A Gradle plugin and related utilities for the Maven Reflections plugin port.
  *
  * @author Alex Tymchenko
