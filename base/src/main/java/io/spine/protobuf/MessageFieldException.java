0ab1c4e1085e6d95c15a6a40343e6bca9b918d8d
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2017-08-04 20:53:36 +0300

    Fix serialization warning

--- base/src/main/java/io/spine/protobuf/MessageFieldException.java
+++ base/src/main/java/io/spine/protobuf/MessageFieldException.java
@@ -1,5 +1,6 @@
 package io.spine.protobuf;
 
+import com.google.protobuf.GeneratedMessageV3;
 import com.google.protobuf.Message;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -12,28 +13,31 @@ import static java.lang.String.format;
  */
 public class MessageFieldException extends RuntimeException {
 
-    private final Message protobufMessage;
+    private static final long serialVersionUID = 0L;
+    private final GeneratedMessageV3 protobufMessage;
 
     /**
      * Constructs a {@code MessageFieldException} with the formatted message text.
      *
-     * @param protobufMessage    a Protobuf message object working with a field of which caused
-     *                           an error
-     * @param errorMessageFormat a format string for the error message
-     * @param params             error message parameters
+     * @param protobufMessage
+     *        a Protobuf message object working with a field of which caused an error
+     * @param errorMessageFormat
+     *        a format string for the error message
+     * @param params
+     *        error message parameters
      */
     public MessageFieldException(Message protobufMessage,
                                  String errorMessageFormat,
                                  Object... params) {
         super(format(checkNotNull(errorMessageFormat), params));
-        this.protobufMessage = checkNotNull(protobufMessage);
+        this.protobufMessage = (GeneratedMessageV3)checkNotNull(protobufMessage);
     }
 
     /**
      * Constructs a {@code MessageFieldException} without no message text.
      *
-     * @param protobufMessage a Protobuf message object working with a field of which caused
-     *                        an error
+     * @param protobufMessage
+     *        a Protobuf message object working with a field of which caused an error
      */
     public MessageFieldException(Message protobufMessage) {
         this(protobufMessage, "");
