22349b8164ea80e36e8fb55bf3f5a5a78f129046
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2017-08-11 00:38:33 +0300

    Fix imports.

--- gradle-plugins/model-compiler/src/main/java/io/spine/gradle/compiler/lookup/enrichment/EnrichmentsFinder.java
+++ gradle-plugins/model-compiler/src/main/java/io/spine/gradle/compiler/lookup/enrichment/EnrichmentsFinder.java
@@ -42,11 +42,11 @@ import java.util.regex.Pattern;
 
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.Lists.newLinkedList;
-import static io.spine.gradle.compiler.util.UnknownOptions.getUnknownOptionValue;
-import static io.spine.gradle.compiler.util.UnknownOptions.hasUnknownOption;
 import static io.spine.option.OptionsProto.BY_FIELD_NUMBER;
 import static io.spine.option.OptionsProto.ENRICHMENT_FIELD_NUMBER;
 import static io.spine.option.OptionsProto.ENRICHMENT_FOR_FIELD_NUMBER;
+import static io.spine.protobuf.UnknownOptions.getUnknownOptionValue;
+import static io.spine.protobuf.UnknownOptions.hasUnknownOption;
 import static java.lang.String.format;
 import static java.util.regex.Pattern.compile;
 
