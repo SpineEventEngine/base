a567a0ac07eca4be71b15db4ce093c389e9036cf
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2018-03-01 18:58:49 +0200

    Remove mentioning immutability from Javadoc
    ... since we standardize on it in [Java Code Style](https://github.com/SpineEventEngine/core-java/wiki/Java-Code-Style#returning-immutable-collections).

--- base/src/main/java/io/spine/util/PropertyFiles.java
+++ base/src/main/java/io/spine/util/PropertyFiles.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, TeamDev Ltd. All rights reserved.
+ * Copyright 2018, TeamDev Ltd. All rights reserved.
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -56,7 +56,7 @@ public class PropertyFiles {
      * <p>Logs {@link IOException} if it occurs.
      *
      * @param propsFilePath the path of the {@code .properties} file to load
-     * @return immutable set with loaded data
+     * @return set with loaded data
      */
     public static Set<Properties> loadAllProperties(String propsFilePath) {
         checkNotNull(propsFilePath);
