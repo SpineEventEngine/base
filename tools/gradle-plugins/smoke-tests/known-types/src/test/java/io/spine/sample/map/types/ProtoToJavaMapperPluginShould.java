04428d32ce55de956a824032641f0acafe1dc927
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2018-03-23 19:20:03 +0200

    Shorten smoke test module names

--- tools/gradle-plugins/smoke-tests/known-types/src/test/java/io/spine/sample/map/types/ProtoToJavaMapperPluginShould.java
+++ tools/gradle-plugins/smoke-tests/known-types/src/test/java/io/spine/sample/map/types/ProtoToJavaMapperPluginShould.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, TeamDev Ltd. All rights reserved.
+ * Copyright 2018, TeamDev Ltd. All rights reserved.
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -22,10 +22,10 @@ package io.spine.sample.map.types;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.FluentIterable;
-import org.junit.Test;
 import io.spine.type.ClassName;
 import io.spine.type.KnownTypes;
 import io.spine.type.TypeUrl;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
