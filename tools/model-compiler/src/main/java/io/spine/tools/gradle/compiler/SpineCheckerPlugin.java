3a1a4c8041c578a0fe84ceb29062543abad92647
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-08-07 22:44:10 +0300

    Reword doc

--- tools/model-compiler/src/main/java/io/spine/tools/gradle/compiler/SpineCheckerPlugin.java
+++ tools/model-compiler/src/main/java/io/spine/tools/gradle/compiler/SpineCheckerPlugin.java
@@ -126,8 +126,8 @@ public class SpineCheckerPlugin extends SpinePlugin {
      * Creates the {@code annotationProcessor} config for the project if it does not exist and
      * adds it to the Java Compiler preprocessor path.
      *
-     * <p>For newer Gradle versions ({@code 4.6} and above) this method will likely just acquire
-     * the config and return it.
+     * <p>For newer Gradle versions ({@code 4.6} and above) this method will most probably just
+     * acquire the config and return it.
      *
      * @param project the project
      * @return the {@code annotationProcessor} configuration of the project
