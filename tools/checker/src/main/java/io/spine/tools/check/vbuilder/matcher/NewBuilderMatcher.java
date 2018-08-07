477dfa07f82ca3159c85780d91b926341928e7a1
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-08-07 16:08:57 +0300

    Simplify newBuilder matcher

--- tools/checker/src/main/java/io/spine/tools/check/vbuilder/matcher/NewBuilderMatcher.java
+++ tools/checker/src/main/java/io/spine/tools/check/vbuilder/matcher/NewBuilderMatcher.java
@@ -23,13 +23,12 @@ package io.spine.tools.check.vbuilder.matcher;
 import com.google.errorprone.VisitorState;
 import com.google.errorprone.matchers.Matcher;
 import com.google.errorprone.matchers.method.MethodMatchers.MethodNameMatcher;
+import com.google.errorprone.predicates.TypePredicate;
 import com.google.errorprone.suppliers.Supplier;
 import com.google.protobuf.Message;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.MethodInvocationTree;
-import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.code.Type;
-import com.sun.tools.javac.tree.JCTree;
 import io.spine.annotation.Internal;
 import io.spine.tools.check.BugPatternMatcher;
 import io.spine.tools.check.Fixer;
@@ -37,7 +36,6 @@ import io.spine.tools.check.vbuilder.fixer.NewBuilderFixer;
 
 import static com.google.errorprone.matchers.method.MethodMatchers.staticMethod;
 import static com.google.errorprone.suppliers.Suppliers.typeFromClass;
-import static com.google.errorprone.util.ASTHelpers.enclosingClass;
 import static com.google.errorprone.util.ASTHelpers.isSubtype;
 
 @Internal
@@ -50,18 +48,8 @@ public class NewBuilderMatcher implements BugPatternMatcher<MethodInvocationTree
 
     @Override
     public boolean matches(MethodInvocationTree tree, VisitorState state) {
-        boolean methodNameMatches = methodNameMatcher().matches(tree, state);
-        ExpressionTree methodSelect = tree.getMethodSelect();
-        boolean calledOnClass = methodSelect instanceof JCTree.JCFieldAccess;
-        boolean staticImported = methodSelect instanceof JCTree.JCIdent;
-        if (!methodNameMatches || (!calledOnClass && !staticImported)) {
-            return false;
-        }
-        Type type = getTypeOnWhichInvoked(tree);
-        Supplier<Type> typeSupplier = typeFromClass(Message.class);
-        Type messageClassAsType = typeSupplier.get(state);
-        boolean invokedOnMessage = isSubtype(type, messageClassAsType, state);
-        return invokedOnMessage;
+        boolean matches = matcher().matches(tree, state);
+        return matches;
     }
 
     @Override
@@ -69,23 +57,14 @@ public class NewBuilderMatcher implements BugPatternMatcher<MethodInvocationTree
         return fixer;
     }
 
-    Type getTypeOnWhichInvoked(MethodInvocationTree tree) {
-        ExpressionTree expression = tree.getMethodSelect();
-        if (expression instanceof JCTree.JCFieldAccess) {
-            JCTree.JCFieldAccess fieldAccess = (JCTree.JCFieldAccess) expression;
-            JCTree.JCExpression invokedOn = fieldAccess.selected;
-            return invokedOn.type;
-        }
-
-        // Static imported method.
-        JCTree.JCIdent ident = (JCTree.JCIdent) expression;
-        Symbol.MethodSymbol method = (Symbol.MethodSymbol) ident.sym;
-        Symbol.ClassSymbol classSymbol = enclosingClass(method);
-        return classSymbol.type;
-    }
-
-    private static Matcher<ExpressionTree> methodNameMatcher() {
-        MethodNameMatcher matcher = staticMethod().anyClass()
+    private static Matcher<ExpressionTree> matcher() {
+        TypePredicate messageSubtype = (type, state) -> {
+            Supplier<Type> typeSupplier = typeFromClass(Message.class);
+            Type messageClassAsType = typeSupplier.get(state);
+            boolean isMessageSubclass = isSubtype(type, messageClassAsType, state);
+            return isMessageSubclass;
+        };
+        MethodNameMatcher matcher = staticMethod().onClass(messageSubtype)
                                                   .named(METHOD_NAME);
         return matcher;
     }
