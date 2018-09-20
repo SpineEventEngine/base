462f83af1f04cb527742bc9ba27a5d8847a7f931
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2018-09-20 20:00:34 +0300

    Revert accidentally disabled test

--- base/src/test/java/io/spine/code/java/SourceFileTest.java
+++ base/src/test/java/io/spine/code/java/SourceFileTest.java
@@ -26,7 +26,6 @@ import com.google.protobuf.Descriptors.Descriptor;
 import io.spine.test.code.NoOuterClassnameSourceFileTest.NoOuterClassnameMessage;
 import io.spine.test.code.SourceFile.NestedMessage;
 import io.spine.test.code.StandaloneMessage;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -39,8 +38,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @author Dmytro Dashenkov
  */
 @DisplayName("Java SourceFile should")
-@SuppressWarnings("InnerClassMayBeStatic") // Nested test suite.
-@Disabled
+@SuppressWarnings("InnerClassMayBeStatic")
+        // Nested test suite.
 class SourceFileTest {
 
     @DisplayName("compile path to class generated from message which")
@@ -86,7 +85,8 @@ class SourceFileTest {
     private static void checkPath(String expectedName, Descriptor descriptor) {
         DescriptorProto message = descriptor.toProto();
 
-        FileDescriptorProto file = descriptor.getFile().toProto();
+        FileDescriptorProto file = descriptor.getFile()
+                                             .toProto();
         SourceFile sourceFile = SourceFile.forMessage(message, file);
         assertEquals(expectedName, sourceFile.toString());
     }
