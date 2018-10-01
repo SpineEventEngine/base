3dae4dbc23738ee5fc09d47e9eaec21842b93d74
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2018-10-01 18:31:40 +0300

    Test `EnumStringifier`

---
+++ base/src/test/java/io/spine/string/EnumStringifierTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2018, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.string;
+
+import com.google.common.base.Converter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("EnumStringifier should")
+class EnumStringifierTest {
+
+    @Test
+    @DisplayName("convert values to String and back")
+    void convert() {
+        DayOfWeekStringifier stringifier = DayOfWeekStringifier.INSTANCE;
+        Converter<String, DayOfWeek> reverse = stringifier.reverse();
+
+        for (DayOfWeek value : DayOfWeek.values()) {
+            String str = stringifier.convert(value);
+            assertEquals(value, reverse.convert(str));
+        }
+    }
+
+
+    private enum DayOfWeek {
+        MONDAY, TUESDAY, WEDNESDAY,
+        THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+
+    private static final class DayOfWeekStringifier extends EnumStringifier<DayOfWeek> {
+
+        private static final long serialVersionUID = 0L;
+        private static final DayOfWeekStringifier INSTANCE = new DayOfWeekStringifier();
+
+        private DayOfWeekStringifier() {
+            super(DayOfWeekStringifier.class.getName(), DayOfWeek.class);
+        }
+    }
+}
