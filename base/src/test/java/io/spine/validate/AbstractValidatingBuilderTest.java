d5844565dd5fdafd7bc580cbaaa375505739f948
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2018-12-12 21:39:42 +0200

    Add more tests into list and map conversion

--- base/src/test/java/io/spine/validate/AbstractValidatingBuilderTest.java
+++ base/src/test/java/io/spine/validate/AbstractValidatingBuilderTest.java
@@ -20,9 +20,17 @@
 
 package io.spine.validate;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.truth.MapSubject;
 import com.google.protobuf.Int32Value;
+import com.google.protobuf.Timestamp;
 import com.google.protobuf.UInt32Value;
+import com.google.protobuf.util.Timestamps;
 import io.spine.base.ConversionException;
+import io.spine.base.Time;
+import io.spine.protobuf.Durations2;
+import io.spine.string.Stringifiers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -30,67 +38,122 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Map;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.truth.Truth.assertThat;
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("AbstractValidatingBuilder should")
 class AbstractValidatingBuilderTest {
 
     @Nested
-    @DisplayName("convert a raw String to")
+    @DisplayName("Convert a raw String to")
     class Convert {
 
-        @Test
+        Joiner joiner = Joiner.on(',');
+
+        @Nested
         @DisplayName("Map")
-        void toMap() {
-            String key1 = "key1";
-            UInt32Value value = UInt32Value.newBuilder()
-                                           .setValue(123)
-                                           .build();
-            String mapStr = "\"key1\":\"123\",\"key2\":\"234\"";
+        class OfMap {
 
-            UInt32ValueVBuilder uInt32ValueVBuilder = UInt32ValueVBuilder.newBuilder();
-            Map<String, UInt32Value> convertedValue =
-                    uInt32ValueVBuilder.convertToMap(mapStr,
-                                                     String.class,
-                                                     UInt32Value.class);
+            String entry(Object key, Object value) {
+                return format("\"%s\":\"%s\"", key, value);
+            }
 
-            assertTrue(convertedValue.containsKey(key1));
-            assertTrue(convertedValue.containsValue(value));
+            @Test
+            @DisplayName("from string key-value")
+            void toMap() {
+                String mapStr = joiner.join(entry("key1", "123"),
+                                            entry("key2", "234"));
+
+                UInt32ValueVBuilder builder = UInt32ValueVBuilder.newBuilder();
+                Map<String, UInt32Value> map =
+                        builder.convertToMap(mapStr, String.class, UInt32Value.class);
+
+                MapSubject assertMap = assertThat(map);
+
+                assertMap.containsEntry(
+                        "key1", UInt32Value
+                                .newBuilder()
+                                .setValue(123)
+                                .build()
+                );
+
+                assertMap.containsEntry(
+                        "key2", UInt32Value
+                                .newBuilder()
+                                .setValue(234)
+                                .build()
+                );
+            }
         }
 
-        @Test
+        @Nested
         @DisplayName("List")
-        void toList() {
-            String key1 = "key1";
-            String value = "123";
-            String listStr = "\"key1\",\"123\",\"key2\",\"234\"";
+        class ToList {
 
-            StringValueVBuilder stringValueVBuilder = StringValueVBuilder.newBuilder();
-            List<String> convertedValue = stringValueVBuilder.convertToList(listStr,
-                                                                            String.class);
+            String item(Object o) {
+                return format("\"%s\"", Stringifiers.toString(o));
+            }
 
-            assertTrue(convertedValue.contains(key1));
-            assertTrue(convertedValue.contains(value));
+            /**
+             * Creates a test input string from the list of objects.
+             */
+            private String createInput(ImmutableList<?> items) {
+                ImmutableList<String> collect = items.stream()
+                                                     .map(this::item)
+                                                     .collect(toImmutableList());
+
+                return joiner.join(collect);
+            }
+
+            @Test
+            @DisplayName("of strings")
+            void toList() {
+                ImmutableList<String> items =
+                        ImmutableList.of("something", "entry1", "anything", "entry2");
+
+                String str = createInput(items);
+
+                StringValueVBuilder builder = StringValueVBuilder.newBuilder();
+                List<String> list = builder.convertToList(str, String.class);
+
+                assertThat(list).containsAllIn(items);
+            }
+
+            @Test
+            @DisplayName("of messages")
+            void toMessageList() {
+                Timestamp now = Time.getCurrentTime();
+                Timestamp then = Timestamps.add(now, Durations2.minutes(5));
+                ImmutableList<Timestamp> times = ImmutableList.of(now, then);
+
+                String str = createInput(times);
+
+                TimestampVBuilder builder = TimestampVBuilder.newBuilder();
+                List<Timestamp> list = builder.convertToList(str, Timestamp.class);
+
+                assertThat(list).containsAllIn(times);
+            }
         }
 
         @Test
-        @DisplayName("Class")
-        void toClass() throws ConversionException {
+        @DisplayName("Instance of a class")
+        void toInstance() throws ConversionException {
             String value = "123";
-            StringValueVBuilder vBuilder = StringValueVBuilder.newBuilder();
-            Int32Value convertedValue = vBuilder.convert(value, Int32Value.class);
+            StringValueVBuilder builder = StringValueVBuilder.newBuilder();
+            Int32Value convertedValue = builder.convert(value, Int32Value.class);
             assertEquals(Integer.parseInt(value), convertedValue.getValue());
         }
+    }
 
-        @Test
-        @DisplayName("wrong Class and fail")
-        void toWrongClass() {
-            String notInt = "notInt";
-            StringValueVBuilder vBuilder = StringValueVBuilder.newBuilder();
-            assertThrows(ConversionException.class,
-                         () -> vBuilder.convert(notInt, Int32Value.class));
-        }
+    @Test
+    @DisplayName("Fail conversion of a string if non-matching class passed")
+    void toWrongClass() {
+        String notInt = "notInt";
+        StringValueVBuilder builder = StringValueVBuilder.newBuilder();
+        assertThrows(ConversionException.class,
+                     () -> builder.convert(notInt, Int32Value.class));
     }
 }
