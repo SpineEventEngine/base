37648cbeb64a187fdae100760538fd601cb3b880
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2018-12-12 20:47:04 +0200

    Make conversion methods final

--- base/src/main/java/io/spine/validate/AbstractValidatingBuilder.java
+++ base/src/main/java/io/spine/validate/AbstractValidatingBuilder.java
@@ -121,7 +121,9 @@ public abstract class AbstractValidatingBuilder<T extends Message, B extends Mes
      *         the {@code Class} of the value
      * @return the converted value
      */
-    protected <K, V> Map<K, V> convertToMap(String value, Class<K> keyClass, Class<V> valueClass) {
+    protected final <K, V> Map<K, V> convertToMap(String value,
+                                                  Class<K> keyClass,
+                                                  Class<V> valueClass) {
         Map<K, V> result = Stringifiers.newForMapOf(keyClass, valueClass)
                                        .reverse()
                                        .convert(value);
@@ -141,7 +143,7 @@ public abstract class AbstractValidatingBuilder<T extends Message, B extends Mes
      *         the {@code Class} of the list values
      * @return the converted value
      */
-    protected <V> List<V> convertToList(String value, Class<V> valueClass) {
+    protected final <V> List<V> convertToList(String value, Class<V> valueClass) {
         List<V> result = Stringifiers.newForListOf(valueClass)
                                      .reverse()
                                      .convert(value);
