e39fc7eb6cdeb87c79a76cf8098ee158cbfa09c5
Serhii Lekariev <serhii.lekariev@teamdev.com>
2018-12-20 13:10:58 +0200

    Change the logic of how `FieldValueChange`s are treated when the change is actually the first time the field is being set

--- base/src/main/java/io/spine/validate/FloatFieldValidator.java
+++ base/src/main/java/io/spine/validate/FloatFieldValidator.java
@@ -20,6 +20,8 @@
 
 package io.spine.validate;
 
+import com.google.common.annotations.VisibleForTesting;
+
 import static java.lang.Math.abs;
 
 /**
@@ -33,6 +35,7 @@ class FloatFieldValidator extends FloatFieldValidatorBase<Float> {
      * @param fieldValue
      *         the value to validate
      */
+    @VisibleForTesting
     FloatFieldValidator(FieldValue fieldValue) {
         super(fieldValue);
     }
