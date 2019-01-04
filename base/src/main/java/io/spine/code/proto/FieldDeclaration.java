720784fea7d722fd0af5b73541aaf490e9938b77
Serhii Lekariev <serhii.lekariev@teamdev.com>
2019-01-04 12:10:14 +0200

    Fix access modifier

--- base/src/main/java/io/spine/code/proto/FieldDeclaration.java
+++ base/src/main/java/io/spine/code/proto/FieldDeclaration.java
@@ -261,7 +261,7 @@ public final class FieldDeclaration implements Logging {
      * @return {@code true} if the field is the first in the containing declaration,
      *         {@code false} otherwise
      */
-    public boolean isFirstField() {
+    private boolean isFirstField() {
         return field.getIndex() == 0;
     }
 
