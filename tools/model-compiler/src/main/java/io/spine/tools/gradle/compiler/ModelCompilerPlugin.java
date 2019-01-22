f098a813a5c24dcdce5a474b2cd902a7cb1b7c1e
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-01-22 12:23:06 +0200

    Delete `spine-protoc.gradle` template and it's supporting plugin.

--- tools/model-compiler/src/main/java/io/spine/tools/gradle/compiler/ModelCompilerPlugin.java
+++ tools/model-compiler/src/main/java/io/spine/tools/gradle/compiler/ModelCompilerPlugin.java
@@ -59,7 +59,6 @@ public class ModelCompilerPlugin implements Plugin<Project>, Logging {
                   new ValidatingBuilderGenPlugin(),
                   new ProtoAnnotatorPlugin(),
                   new ValidationRulesLookupPlugin(),
-//                  new ProtocPluginImporter(),
                   new ProtocConfigurationPlugin(),
                   new DescriptorSetMergerPlugin(),
                   new ErrorProneChecksPlugin())
