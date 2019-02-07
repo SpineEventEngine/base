a0a29c7fea5f1b494fe2cd9a6fb2a6d1f8a9d240
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2019-02-07 18:42:35 +0200

    Improve warning diags

--- base/src/main/java/io/spine/validate/Required.java
+++ base/src/main/java/io/spine/validate/Required.java
@@ -105,7 +105,11 @@ class Required<T> extends FieldValidatingOption<Boolean, T> implements Logging {
         JavaType type = field.getJavaType();
         if (!CAN_BE_REQUIRED.contains(type)) {
             String typeName = field.getType().name();
-            _warn("Fields of type {} should not be declared as `(required)`.", typeName);
+            _warn("Fields of type {} should not be declared as `(required)`. " +
+                          "Please see the declaration of `%s.%s`.",
+                  typeName,
+                  field.getContainingType().getFullName(),
+                  field.getName());
         }
     }
 
