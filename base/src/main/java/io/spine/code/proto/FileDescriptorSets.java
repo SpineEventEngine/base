7f3f389880f7d1aa5ab05e455aac186a7f6f70b3
Serhii Lekariev <serhii.lekariev@teamdev.com>
2019-02-13 12:37:37 +0200

    Get rid of all `Options.option()` method calls.

--- base/src/main/java/io/spine/code/proto/FileDescriptorSets.java
+++ base/src/main/java/io/spine/code/proto/FileDescriptorSets.java
@@ -22,12 +22,12 @@ package io.spine.code.proto;
 
 import com.google.protobuf.DescriptorProtos.FileDescriptorSet;
 import com.google.protobuf.InvalidProtocolBufferException;
+import io.spine.option.OptionExtensionRegistry;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Optional;
 
-import static io.spine.option.Options.registry;
 import static io.spine.util.Exceptions.illegalArgumentWithCauseOf;
 
 /**
@@ -52,7 +52,7 @@ public final class FileDescriptorSets {
      */
     public static FileDescriptorSet parse(byte[] bytes) {
         try {
-            return FileDescriptorSet.parseFrom(bytes, registry());
+            return FileDescriptorSet.parseFrom(bytes, OptionExtensionRegistry.obtain());
         } catch (InvalidProtocolBufferException e) {
             throw illegalArgumentWithCauseOf(e);
         }
@@ -68,7 +68,7 @@ public final class FileDescriptorSets {
      */
     public static Optional<FileDescriptorSet> tryParse(byte[] bytes) {
         try {
-            FileDescriptorSet descriptorSet = FileDescriptorSet.parseFrom(bytes, registry());
+            FileDescriptorSet descriptorSet = FileDescriptorSet.parseFrom(bytes, OptionExtensionRegistry.obtain());
             return Optional.of(descriptorSet);
         } catch (InvalidProtocolBufferException e) {
             return Optional.empty();
@@ -86,7 +86,7 @@ public final class FileDescriptorSets {
      */
     public static FileDescriptorSet parse(InputStream stream) {
         try {
-            return FileDescriptorSet.parseFrom(stream, registry());
+            return FileDescriptorSet.parseFrom(stream, OptionExtensionRegistry.obtain());
         } catch (IOException e) {
             throw illegalArgumentWithCauseOf(e);
         }
