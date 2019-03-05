142d4fe6387056b985cbea2809123601dcb0c1be
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2019-03-05 02:05:48 +0200

    Improve test of obtaining interfaces in the hierarchy

--- base/src/test/java/io/spine/type/MessageClassTest.java
+++ base/src/test/java/io/spine/type/MessageClassTest.java
@@ -21,11 +21,19 @@
 package io.spine.type;
 
 import com.google.common.testing.EqualsTester;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.CodedOutputStream;
+import com.google.protobuf.Descriptors;
 import com.google.protobuf.Message;
+import com.google.protobuf.Parser;
 import com.google.protobuf.StringValue;
-import io.spine.test.types.MtDummy;
+import com.google.protobuf.UnknownFieldSet;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
 
 import static com.google.common.testing.SerializableTester.reserializeAndAssert;
 import static com.google.common.truth.Truth.assertThat;
@@ -61,12 +69,12 @@ class MessageClassTest {
     @Test
     @DisplayName("obtain super interfaces of a class")
     void interfaces() {
-        assertThat(MessageClass.interfacesOf(MtDummy.class))
-                .isEmpty();
+        assertThat(MessageClass.interfacesOf(StubMessage.class))
+                .containsExactly(SubSubMessage.class, SubMessage.class, SuperMessage.class);
     }
 
     /**
-     * Test environment class.
+     * Test environment class extending the abstract base.
      */
     private static class TestMessageClass extends MessageClass<Message> {
 
@@ -74,6 +82,133 @@ class MessageClassTest {
 
         private TestMessageClass(Class<? extends Message> value) {
             super(value);
+        }
+    }
+
+    /**
+     * Base test environment interface for {@link MessageClassTest#interfaces()}.
+     */
+    private interface SuperMessage extends Message {
+    }
+
+    private interface SubMessage extends SuperMessage {
+    }
+
+    private interface SubSubMessage extends SubMessage {
+    }
+
+    /**
+     * Stub implementation for {@link MessageClassTest#interfaces()}.
+     */
+    private static final class StubMessage implements SubSubMessage {
+
+        @Override
+        public void writeTo(CodedOutputStream output) {
+        }
+
+        @Override
+        public int getSerializedSize() {
+            return 0;
+        }
+
+        @Override
+        public Parser<? extends Message> getParserForType() {
+            return null;
+        }
+
+        @Override
+        public ByteString toByteString() {
+            return null;
+        }
+
+        @SuppressWarnings("ZeroLengthArrayAllocation")
+        @Override
+        public byte[] toByteArray() {
+            return new byte[0];
+        }
+
+        @Override
+        public void writeTo(OutputStream output) {
+        }
+
+        @Override
+        public void writeDelimitedTo(OutputStream output) {
+        }
+
+        @Override
+        public Builder newBuilderForType() {
+            return null;
+        }
+
+        @Override
+        public Builder toBuilder() {
+            return null;
+        }
+
+        @Override
+        public Message getDefaultInstanceForType() {
+            return null;
+        }
+
+        @Override
+        public boolean isInitialized() {
+            return false;
+        }
+
+        @Override
+        public List<String> findInitializationErrors() {
+            return null;
+        }
+
+        @Override
+        public String getInitializationErrorString() {
+            return null;
+        }
+
+        @Override
+        public Descriptors.Descriptor getDescriptorForType() {
+            return null;
+        }
+
+        @Override
+        public Map<Descriptors.FieldDescriptor, Object> getAllFields() {
+            return null;
+        }
+
+        @Override
+        public boolean hasOneof(Descriptors.OneofDescriptor oneof) {
+            return false;
+        }
+
+        @Override
+        public Descriptors.FieldDescriptor getOneofFieldDescriptor(
+                Descriptors.OneofDescriptor oneof) {
+            return null;
+        }
+
+        @Override
+        public boolean hasField(Descriptors.FieldDescriptor field) {
+            return false;
+        }
+
+        @Override
+        public Object getField(Descriptors.FieldDescriptor field) {
+            return null;
+        }
+
+        @Override
+        public int getRepeatedFieldCount(Descriptors.FieldDescriptor field) {
+            return 0;
+        }
+
+        @Override
+        public Object getRepeatedField(Descriptors.FieldDescriptor field, int index) {
+            return null;
+        }
+
+        @Override
+        public UnknownFieldSet getUnknownFields() {
+            return null;
         }
     }
 }
