d1c528a1d998fd1104018c8f7c9a65e19c645b87
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2019-03-11 09:56:01 +0200

    Fixate MethodFactory contract

---
+++ base/src/test/java/io/spine/tools/method/MethodFactoryTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.tools.method;
+
+import com.google.common.collect.ImmutableList;
+import com.google.errorprone.annotations.Immutable;
+import io.spine.tools.protoc.method.GeneratedMethod;
+import io.spine.tools.protoc.method.MethodFactory;
+import io.spine.type.MessageType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+
+/**
+ * With this unit test we fixating {@link MethodFactory} contract.
+ */
+@DisplayName("MethodFactory should")
+final class MethodFactoryTest {
+
+    @DisplayName("have specific contract")
+    @Test
+    void shouldGenerateNewMethod() {
+        assertThat(new TestMethodFactory().newMethodsFor(null)).isEmpty();
+    }
+
+    @Immutable
+    public static class TestMethodFactory implements MethodFactory {
+
+        public TestMethodFactory() {
+        }
+
+        @Override
+        public List<GeneratedMethod> newMethodsFor(MessageType messageType) {
+            return ImmutableList.of();
+        }
+    }
+
+}
