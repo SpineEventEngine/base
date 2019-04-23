43ef3f8354d18bb639c28b9ada6152ae9d7ac216
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2019-04-23 16:54:53 +0300

    Add validation rules test

---
+++ base/src/test/java/io/spine/validate/rule/ValidationRulesTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.validate.rule;
+
+import com.google.common.collect.ImmutableSet;
+import io.spine.test.validate.rule.AValidationRule;
+import io.spine.type.MessageType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@DisplayName("ValidationRules should")
+final class ValidationRulesTest {
+
+    @DisplayName("update rules from types")
+    @Test
+    void updateRulesFromTypes() {
+        MessageType ruleType = new MessageType(AValidationRule.getDescriptor());
+        ValidationRules.updateFrom(ImmutableSet.of(ruleType));
+        assertThat(ValidationRules.all()).hasSize(1);
+    }
+}
