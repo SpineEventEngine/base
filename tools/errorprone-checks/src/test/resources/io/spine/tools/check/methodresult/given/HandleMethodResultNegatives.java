33dce212a93efa4d4e4cc5e02c163e52b77ecf8d
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-05-28 17:41:48 +0300

    Fix test data

--- tools/errorprone-checks/src/test/resources/io/spine/tools/check/methodresult/given/HandleMethodResultNegatives.java
+++ tools/errorprone-checks/src/test/resources/io/spine/tools/check/methodresult/given/HandleMethodResultNegatives.java
@@ -18,7 +18,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package io.spine.tools.check.vbuild;
+package io.spine.tools.check.vbuild.given;
 
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import com.google.protobuf.Struct;
