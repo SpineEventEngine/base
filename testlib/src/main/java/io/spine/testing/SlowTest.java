325ff10d935a3283f329ede423fd6c59bd0acc9c
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-07-04 19:45:41 +0300

    Define `SlowTest`

---
+++ testlib/src/main/java/io/spine/testing/SlowTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.testing;
+
+import org.junit.jupiter.api.Tag;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Marks tests which are known to be slow and should not normally be run together with the main test
+ * suite.
+ *
+ * <p>Slow tests typically are functional test, which may call network API, perform I/O operations,
+ * spawn many threads and wait for execution, etc.
+ *
+ * <p>This annotation is an alias for {@code Tag("slow")}. Adding the {@code slow} tag on a test
+ * case produces the same effect as adding this annotation.
+ */
+@Retention(RUNTIME)
+@Target({TYPE, METHOD})
+@Tag(SlowTest.TAG)
+@SuppressWarnings("unused") // The annotation should be handled by the JUnit runner.
+public @interface SlowTest {
+
+    String TAG = "slow";
+}
