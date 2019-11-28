6c4127e92241221c25c20448877a3eb05f70276e
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-11-28 12:19:42 +0200

    Add some more tests for `(required)`

--- tools/smoke-tests/generated-validation-tests/src/test/java/io/spine/test/tools/validate/RequiredConstraintTest.java
+++ tools/smoke-tests/generated-validation-tests/src/test/java/io/spine/test/tools/validate/RequiredConstraintTest.java
@@ -21,6 +21,7 @@
 package io.spine.test.tools.validate;
 
 import com.google.protobuf.ByteString;
+import com.google.protobuf.Empty;
 import io.spine.base.Identifier;
 import io.spine.protobuf.MessageWithConstraints;
 import io.spine.validate.ConstraintViolation;
@@ -123,12 +124,134 @@ class RequiredConstraintTest {
         checkNoViolation(singulars, "not_default");
     }
 
+    @Test
+    @DisplayName("google.protobuf.Empty cannot be a required field")
+    void notAllowEmptyRequired() {
+        AlwaysInvalid unset = AlwaysInvalid
+                .newBuilder()
+                .build();
+        checkViolation(unset, "impossible");
+        AlwaysInvalid set = AlwaysInvalid
+                .newBuilder()
+                .setImpossible(Empty.getDefaultInstance())
+                .build();
+        checkViolation(set, "impossible");
+    }
+
+    @Test
+    @DisplayName("all violations on a single message are collected")
+    void collectManyViolations() {
+        Singulars instance = Singulars.getDefaultInstance();
+        List<ConstraintViolation> violations = instance.validate();
+        assertThat(violations).hasSize(4);
+    }
+
+    @Test
+    @DisplayName("repeated number fields must have at least one value")
+    void emptyRepeatedInt() {
+        Collections instance = Collections.getDefaultInstance();
+        checkCollectionViolation(instance,"not_empty_list_of_longs");
+    }
+
+    @Test
+    @DisplayName("elements of repeated number fields do not matter")
+    void repeatedInt() {
+        Collections instance = Collections
+                .newBuilder()
+                .addNotEmptyListOfLongs(0L)
+                .buildPartial();
+        checkNoViolation(instance,"not_empty_list_of_longs");
+    }
+
+    @Test
+    @DisplayName("map number fields must have at least one value")
+    void emptyMapOfInts() {
+        Collections instance = Collections.getDefaultInstance();
+        checkCollectionViolation(instance,"not_empty_map_of_ints");
+    }
+
+    @Test
+    @DisplayName("elements of map number fields do not matter")
+    void mapOfInts() {
+        Collections instance = Collections
+                .newBuilder()
+                .putNotEmptyMapOfInts(0, 0)
+                .buildPartial();
+        checkNoViolation(instance,"not_empty_map_of_ints");
+    }
+
+    @Test
+    @DisplayName("map string fields must have at least one value")
+    void emptyMapOfStrings() {
+        Collections instance = Collections.getDefaultInstance();
+        checkCollectionViolation(instance,"contains_a_non_empty_string_value");
+    }
+
+    @Test
+    @DisplayName("map string fields must have at least one non-empty value")
+    void mapOfEmptyStrings() {
+        Collections instance = Collections
+                .newBuilder()
+                .putContainsANonEmptyStringValue("", "")
+                .buildPartial();
+        checkViolation(instance,"contains_a_non_empty_string_value");
+    }
+
+    @Test
+    @DisplayName("a non-empty map of non-empty strings is accepted")
+    void mapOfStrings() {
+        Collections instance = Collections
+                .newBuilder()
+                .putContainsANonEmptyStringValue("", " ")
+                .buildPartial();
+        checkNoViolation(instance,"contains_a_non_empty_string_value");
+    }
+
+    @Test
+    @DisplayName("an empty repeated field of enums is a violation")
+    void emptyRepeatedEnum() {
+        Collections instance = Collections.getDefaultInstance();
+        checkCollectionViolation(instance, "at_least_one_piece_of_meat");
+    }
+
+    @Test
+    @DisplayName("an repeated field of default enums is a violation")
+    void repeatedDefaultEnum() {
+        Collections instance = Collections
+                .newBuilder()
+                .addAtLeastOnePieceOfMeat(UltimateChoice.VEGETABLE)
+                .addAtLeastOnePieceOfMeat(UltimateChoice.VEGETABLE)
+                .buildPartial();
+        checkViolation(instance, "at_least_one_piece_of_meat");
+    }
+
+    @Test
+    @DisplayName("an repeated field of enums is accepted")
+    void repeatedEnum() {
+        Collections instance = Collections
+                .newBuilder()
+                .addAtLeastOnePieceOfMeat(UltimateChoice.FISH)
+                .addAtLeastOnePieceOfMeat(UltimateChoice.VEGETABLE)
+                .buildPartial();
+        checkNoViolation(instance, "at_least_one_piece_of_meat");
+    }
+
+    private static void checkCollectionViolation(MessageWithConstraints message, String field) {
+        checkViolation(message, field, "must not be empty");
+    }
+
     private static void checkViolation(MessageWithConstraints message, String field) {
+        checkViolation(message, field, "must be set");
+    }
+
+    private static void checkViolation(MessageWithConstraints message,
+                                       String field,
+                                       String errorMessagePart) {
         List<ConstraintViolation> violations = message.validate();
         List<ConstraintViolation> stringViolations = violationAtField(violations, field);
         assertThat(stringViolations).hasSize(1);
         ConstraintViolation violation = stringViolations.get(0);
-        assertThat(violation.getMsgFormat()).contains("must be set");
+        assertThat(violation.getMsgFormat()).contains(errorMessagePart);
     }
 
     private static void checkNoViolation(MessageWithConstraints message, String field) {
