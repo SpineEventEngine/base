4461c3460fb2cd85abc4fbc4f35af644db2d2664
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-12-19 15:41:27 +0200

    Add tests for `ConstraintCompiler`

---
+++ tools/validation-generator/src/test/java/io/spine/tools/validate/ConstraintCompilerTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.tools.validate;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.testing.NullPointerTester;
+import com.google.errorprone.annotations.Immutable;
+import com.google.protobuf.Empty;
+import io.spine.code.proto.FieldContext;
+import io.spine.type.MessageType;
+import io.spine.validate.ConstraintViolation;
+import io.spine.validate.CustomConstraint;
+import io.spine.validate.MessageValue;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.testing.NullPointerTester.Visibility.PACKAGE;
+import static io.spine.testing.DisplayNames.NOT_ACCEPT_NULLS;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("`ConstraintCompiler` should")
+class ConstraintCompilerTest {
+
+    @Test
+    @DisplayName(NOT_ACCEPT_NULLS)
+    void notNull() {
+        new NullPointerTester()
+                .setDefault(MessageType.class, FakeConstraint.INSTANCE.targetType())
+                .setDefault(String.class, "nonEmptyString")
+                .testConstructors(ConstraintCompiler.class, PACKAGE);
+    }
+
+    @Test
+    @DisplayName("throw `UnsupportedOperationException` upon `CustomConstraint`")
+    void notAllowCustom() {
+        CustomConstraint constraint = FakeConstraint.INSTANCE;
+        ConstraintCompiler compiler =
+                new ConstraintCompiler("test", constraint.targetType());
+        assertThrows(UnsupportedOperationException.class, () -> compiler.visitCustom(constraint));
+    }
+
+    @Immutable
+    private enum FakeConstraint implements CustomConstraint {
+
+        INSTANCE;
+
+        @Override
+        public ImmutableList<ConstraintViolation> validate(MessageValue containingMessage) {
+            return ImmutableList.of();
+        }
+
+        @Override
+        public MessageType targetType() {
+            return new MessageType(Empty.getDescriptor());
+        }
+
+        @Override
+        public String errorMessage(FieldContext field) {
+            return "";
+        }
+    }
+}
