3dfff527b40f4ae32a024ff2e000d1ca9116fe76
serhii.lekariev <serhii.lekariev@teamdev.com>
2020-06-22 11:29:41 +0300

    Bump the version.

--- version.gradle.kts
+++ version.gradle.kts
@@ -25,7 +25,7 @@
  * as we want to manage the versions in a single source.
  */
 
-val SPINE_VERSION = "1.5.19"
+val SPINE_VERSION = "1.5.20"
 
 project.extra.apply {
     this["spineVersion"] = SPINE_VERSION
