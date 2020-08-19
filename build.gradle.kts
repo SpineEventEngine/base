170daee6f0726cf6dc520dbf6b686722c947c09e
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2020-08-19 15:29:59 +0300

    Publish Javadoc via GitHub Pages

--- build.gradle.kts
+++ build.gradle.kts
@@ -202,7 +202,11 @@ subprojects {
         dependsOn(cleanGenerated)
     }
 
-    apply(from = Deps.scripts.pmd(project))
+    apply {
+        from(Deps.scripts.pmd(project))
+        from(Deps.scripts.updateGitHubPages(project))
+    }
+    project.tasks["publish"].dependsOn("${project.path}:updateGitHubPages")
 }
 
 apply {
