3c752c03e61306d79a7db0fa8b6cd86034a241cf
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2020-10-19 21:57:00 +0300

    Remove hash from the Javadoc link
    Because `errorprone-checks:javadoc` task generates the following error:
    
    Spine/base/tools/errorprone-checks/src/main/java/io/spine/tools/check/vbuild/UseVBuild.java:64: warning - Tag @see: missing final '>': "<a href="https://spine.io/docs/guides/validation.html">Validation Guide</a>
          for more info on model validation in Java."

--- tools/errorprone-checks/src/main/java/io/spine/tools/check/vbuild/UseVBuild.java
+++ tools/errorprone-checks/src/main/java/io/spine/tools/check/vbuild/UseVBuild.java
@@ -49,8 +49,8 @@ import static com.google.errorprone.matchers.Description.NO_MATCH;
  * is what the user wants in most cases. If, however, for some reason, the validation is unwanted,
  * the user in encouraged to use {@code buildPartial()} in order to make the intent explicit.
  *
- * @see <a href="https://spine.io/docs/guides/validation.html#java-validation-api">Validation Guide
- *        </a> for more info on model validation in Java.
+ * @see <a href="https://spine.io/docs/guides/validation.html">Validation Guide</a>
+ *      for more info on model validation in Java.
  */
 @AutoService(BugChecker.class)
 @BugPattern(
