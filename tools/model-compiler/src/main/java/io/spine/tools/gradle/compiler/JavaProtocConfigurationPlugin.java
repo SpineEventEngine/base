ecd6d6589a4c5f1cc3cafaf7141910ec28374c8b
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2020-11-05 15:43:43 +0200

    Use Protobuf plugin instead of bespoke launch scripts

--- tools/model-compiler/src/main/java/io/spine/tools/gradle/compiler/JavaProtocConfigurationPlugin.java
+++ tools/model-compiler/src/main/java/io/spine/tools/gradle/compiler/JavaProtocConfigurationPlugin.java
@@ -21,15 +21,12 @@
 package io.spine.tools.gradle.compiler;
 
 import com.google.common.base.Charsets;
-import com.google.common.io.CharSink;
 import com.google.protobuf.gradle.ExecutableLocator;
 import com.google.protobuf.gradle.GenerateProtoTask;
 import io.spine.code.fs.java.DefaultJavaProject;
 import io.spine.code.fs.java.DefaultJavaProject.GeneratedRoot;
 import io.spine.code.proto.DescriptorReference;
-import io.spine.io.Resource;
 import io.spine.tools.gradle.Artifact;
-import io.spine.tools.gradle.ConfigurationName;
 import io.spine.tools.gradle.GradleTask;
 import io.spine.tools.gradle.ProtocConfigurationPlugin;
 import io.spine.tools.gradle.SourceScope;
@@ -37,25 +34,15 @@ import io.spine.tools.gradle.TaskName;
 import org.gradle.api.NamedDomainObjectContainer;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
-import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.plugins.JavaPluginConvention;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Base64;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import static com.google.common.base.Preconditions.checkState;
-import static com.google.common.io.Files.asCharSink;
 import static io.spine.code.fs.java.DefaultJavaProject.at;
-import static io.spine.io.Resource.file;
 import static io.spine.tools.gradle.BaseTaskName.clean;
-import static io.spine.tools.gradle.ConfigurationName.fetch;
 import static io.spine.tools.gradle.JavaTaskName.processResources;
 import static io.spine.tools.gradle.JavaTaskName.processTestResources;
 import static io.spine.tools.gradle.ModelCompilerTaskName.writeDescriptorReference;
@@ -65,10 +52,6 @@ import static io.spine.tools.gradle.ModelCompilerTaskName.writeTestPluginConfigu
 import static io.spine.tools.gradle.ProtocPluginName.grpc;
 import static io.spine.tools.gradle.ProtocPluginName.spineProtoc;
 import static io.spine.tools.gradle.compiler.ProtocPluginConfiguration.forProject;
-import static io.spine.util.Exceptions.illegalStateWithCauseOf;
-import static java.nio.file.Files.createTempFile;
-import static java.util.regex.Matcher.quoteReplacement;
-import static org.gradle.internal.os.OperatingSystem.current;
 
 /**
  * A Gradle plugin that performs additional {@code protoc} configurations relevant for Java
@@ -79,10 +62,7 @@ public final class JavaProtocConfigurationPlugin extends ProtocConfigurationPlug
     private static final String JAR_EXTENSION = "jar";
     private static final String GRPC_GROUP = "io.grpc";
     private static final String GRPC_PLUGIN_NAME = "protoc-gen-grpc-java";
-    private static final String DOT_SH = ".sh";
-    private static final String DOT_BAT = ".bat";
     private static final String SPINE_PLUGIN_NAME = "spine-protoc-plugin";
-    private static final Pattern JAR_FILE_INSERTION_POINT = Pattern.compile("\\{jar-file}");
 
     @Override
     protected void
@@ -94,22 +74,6 @@ public final class JavaProtocConfigurationPlugin extends ProtocConfigurationPlug
                                                               .setVersion(VERSIONS.grpc())
                                                               .build()
                                                               .notation()));
-        project.afterEvaluate(p -> {
-            Dependency dependency = pluginJarDependency(project);
-            Path jarFile = project.getConfigurations()
-                                  .getByName(fetch.name())
-                                  .fileCollection(dependency)
-                                  .getSingleFile()
-                                  .toPath()
-                                  .toAbsolutePath();
-            plugins.create(spineProtoc.name(),
-                           locator -> locator.setPath(tryWriteLaunchScript(jarFile).toString()));
-        });
-    }
-
-    private static Dependency pluginJarDependency(Project project) {
-        Configuration fetch = project.getConfigurations()
-                                     .maybeCreate(ConfigurationName.fetch.name());
         Artifact protocPluginArtifact = Artifact
                 .newBuilder()
                 .useSpineToolsGroup()
@@ -117,36 +81,10 @@ public final class JavaProtocConfigurationPlugin extends ProtocConfigurationPlug
                 .setVersion(VERSIONS.spineBase())
                 .setExtension(JAR_EXTENSION)
                 .build();
-        Dependency protocPluginDependency = project
-                .getDependencies()
-                .add(fetch.getName(), protocPluginArtifact.notation());
-        return protocPluginDependency;
-    }
-
-    private static Path tryWriteLaunchScript(Path jarFile) {
-        try {
-            return writeLaunchScript(jarFile);
-        } catch (IOException e) {
-            throw illegalStateWithCauseOf(e);
-        }
-    }
-
-    private static Path writeLaunchScript(Path jarFile) throws IOException {
-        boolean windows = current().isWindows();
-        String scriptExt = windows ? DOT_BAT : DOT_SH;
-        Resource launcherTemplate = file("plugin_runner" + scriptExt,
-                                         JavaProtocConfigurationPlugin.class.getClassLoader());
-        String template = launcherTemplate.read();
-        Matcher matcher = JAR_FILE_INSERTION_POINT.matcher(template);
-        String script = matcher.replaceAll(quoteReplacement(jarFile.toString()));
-        Path path = createTempFile(JavaProtocConfigurationPlugin.class.getSimpleName(), scriptExt);
-        // TODO:2020-11-04:dmytro.dashenkov: Kill launch scripts.
-        File file = path.toFile();
-        CharSink sink = asCharSink(file, StandardCharsets.UTF_8);
-        sink.write(script);
-        boolean canBeExecuted = file.setExecutable(true, false);
-        checkState(canBeExecuted, "Failed to make file `%s` executable.", file);
-        return path;
+        project.afterEvaluate(p -> plugins.create(
+                spineProtoc.name(),
+                locator -> locator.setArtifact(protocPluginArtifact.notation()))
+        );
     }
 
     @Override
@@ -244,9 +182,9 @@ public final class JavaProtocConfigurationPlugin extends ProtocConfigurationPlug
     }
 
     private static TaskName spineProtocConfigWriteTaskName(GenerateProtoTask protoTask) {
-        return isTestsTask(protoTask) ?
-               writeTestPluginConfiguration :
-               writePluginConfiguration;
+        return isTestsTask(protoTask)
+               ? writeTestPluginConfiguration
+               : writePluginConfiguration;
     }
 
     private static Path spineProtocConfigPath(GenerateProtoTask protocTask) {
