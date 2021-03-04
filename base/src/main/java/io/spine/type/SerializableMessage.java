1538383cad394bb08339e9b6c90a57a0c3b04c5e
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2021-03-04 21:06:27 +0200

    Move `SerializableMessage` under `type` package

--- base/src/main/java/io/spine/type/SerializableMessage.java
+++ base/src/main/java/io/spine/type/SerializableMessage.java
@@ -24,7 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package io.spine.base;
+package io.spine.type;
 
 import com.google.errorprone.annotations.Immutable;
 import com.google.protobuf.Message;
