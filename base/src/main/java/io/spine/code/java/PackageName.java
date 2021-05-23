a91a2c3c8ef880ca5488ed13219e462036c603b3
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-05-23 17:26:10 +0300

    Improve diagnostics message

--- base/src/main/java/io/spine/code/java/PackageName.java
+++ base/src/main/java/io/spine/code/java/PackageName.java
@@ -108,10 +108,9 @@ public final class PackageName extends StringTypeValue {
         String javaPackage = resolveName(file).trim();
         checkArgument(
                 !javaPackage.isEmpty(),
-                "Message classes generated from the file `%s` belong to the default package.%s"
-                        + "Use `option java_package` or `package` to specify the Java package.",
-                file.getName(),
-                System.lineSeparator()
+                "Message classes generated from the file `%s` belong to the default package.%n"
+                        + "Please use `option java_package` or `package`"
+                        + " to specify a Java package for these types.", file.getName()
         );
         PackageName result = new PackageName(javaPackage);
         return result;
