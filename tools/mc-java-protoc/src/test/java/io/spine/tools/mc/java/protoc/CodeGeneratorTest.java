0066057fc73efd2de24fb4b75564af6b3772a2c1
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2021-06-30 13:00:33 +0300

    Reanimate some tests

--- tools/mc-java-protoc/src/test/java/io/spine/tools/mc/java/protoc/CodeGeneratorTest.java
+++ tools/mc-java-protoc/src/test/java/io/spine/tools/mc/java/protoc/CodeGeneratorTest.java
@@ -27,15 +27,33 @@
 package io.spine.tools.mc.java.protoc;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.truth.IterableSubject;
+import com.google.common.truth.StringSubject;
+import com.google.protobuf.compiler.PluginProtos.CodeGeneratorRequest;
+import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse;
 import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse.File;
+import io.spine.tools.mc.java.protoc.given.TestInterface;
+import io.spine.tools.protoc.ForUuids;
+import io.spine.tools.protoc.SpineProtocConfig;
+import io.spine.tools.protoc.plugin.EnhancedWithCodeGeneration;
+import io.spine.tools.protoc.plugin.TestGeneratorsProto;
+import io.spine.type.MessageType;
 import io.spine.type.Type;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.nio.file.Path;
 import java.util.Collection;
+
+import static com.google.common.truth.Truth.assertThat;
+import static io.spine.testing.Assertions.assertIllegalArgument;
+import static io.spine.testing.Assertions.assertNpe;
+import static io.spine.tools.mc.java.protoc.given.CodeGeneratorRequestGiven.addInterface;
+import static io.spine.tools.mc.java.protoc.given.CodeGeneratorRequestGiven.protocConfig;
+import static io.spine.tools.mc.java.protoc.given.CodeGeneratorRequestGiven.requestBuilder;
 
 @DisplayName("`SpineProtoGenerator` should")
 final class CodeGeneratorTest {
@@ -52,145 +70,147 @@ final class CodeGeneratorTest {
     @DisplayName("process valid `CodeGeneratorRequest`")
     @Test
     void processValidRequest() {
-//        Interfaces interfaces = new Interfaces();
-//        MessageSelectorFactory messages = interfaces.messages();
-//        interfaces.mark(messages.uuid(), ClassName.of(TestInterface.class));
-//        Methods methods = new Methods();
-//        methods.applyFactory(UuidMethodFactory.class.getName(), messages.uuid());
-//        NestedClasses nestedClasses = new NestedClasses();
-//        nestedClasses.applyFactory(TestNestedClassFactory.class.getCanonicalName(),
-//                                   new WithSuffix("*file.proto"));
-//        CodeGeneratorRequest request = requestBuilder()
-//                .addProtoFile(TestGeneratorsProto.getDescriptor()
-//                                                 .toProto())
-//                .addFileToGenerate(TEST_PROTO_FILE)
-//                .setParameter(protocConfig(interfaces, methods, nestedClasses, testPluginConfig))
-//                .build();
-//        MessageType type = new MessageType(EnhancedWithCodeGeneration.getDescriptor());
-//        File firstFile = File
-//                .newBuilder()
-//                .setName("file.proto")
-//                .setContent(TestInterface.class.getName() + ',')
-//                .setInsertionPoint(InsertionPoint.message_implements.forType(type))
-//                .build();
-//        File secondFile = File
-//                .newBuilder()
-//                .setName("file.proto")
-//                .setContent("public void test(){}")
-//                .setInsertionPoint(InsertionPoint.class_scope.forType(type))
-//                .build();
-//        TestGenerator firstGenerator = new TestGenerator(new TestCompilerOutput(firstFile),
-//                                                         new TestCompilerOutput(secondFile));
-//        CodeGeneratorResponse result = firstGenerator.process(request);
-//        assertEquals(2, result.getFileCount());
-//        assertSame(firstFile, result.getFile(0));
-//        assertSame(secondFile, result.getFile(1));
+        ForUuids uuids = ForUuids.newBuilder()
+                .addAddInterface(addInterface(TestInterface.class))
+                .build();
+        SpineProtocConfig config = SpineProtocConfig.newBuilder()
+                .setUuids(uuids)
+                .build();
+        CodeGeneratorRequest request = requestBuilder()
+                .addProtoFile(TestGeneratorsProto.getDescriptor()
+                                                 .toProto())
+                .addFileToGenerate(TEST_PROTO_FILE)
+                .setParameter(protocConfig(config, testPluginConfig))
+                .build();
+        MessageType type = new MessageType(EnhancedWithCodeGeneration.getDescriptor());
+        File firstFile = File
+                .newBuilder()
+                .setName("file.proto")
+                .setContent(TestInterface.class.getName() + ',')
+                .setInsertionPoint(InsertionPoint.message_implements.forType(type))
+                .build();
+        File secondFile = File
+                .newBuilder()
+                .setName("file.proto")
+                .setContent("public void test(){}")
+                .setInsertionPoint(InsertionPoint.class_scope.forType(type))
+                .build();
+        TestGenerator firstGenerator = new TestGenerator(new TestCompilerOutput(firstFile),
+                                                         new TestCompilerOutput(secondFile));
+        CodeGeneratorResponse result = firstGenerator.process(request);
+
+        IterableSubject assertFiles = assertThat(result.getFileList());
+        assertFiles
+                .hasSize(2);
+        assertFiles
+                .containsExactly(firstFile, secondFile);
     }
 
     @DisplayName("concatenate code generated for the same insertion point")
     @Test
     void concatenateGeneratedCode() {
-//        Methods methods = new Methods();
-//        MessageSelectorFactory messages = methods.messages();
-//        methods.applyFactory(UuidMethodFactory.class.getName(), messages.uuid());
-//        CodeGeneratorRequest request = requestBuilder()
-//                .addProtoFile(TestGeneratorsProto.getDescriptor()
-//                                                 .toProto())
-//                .addFileToGenerate(TEST_PROTO_FILE)
-//                .setParameter(protocConfig(methods, testPluginConfig))
-//                .build();
-//        MessageType type = new MessageType(EnhancedWithCodeGeneration.getDescriptor());
-//        String firstMethod = "public void test1(){}";
-//        String secondMethod = "public void test2(){}";
-//        File firstFile = File
-//                .newBuilder()
-//                .setName("file.proto")
-//                .setContent(firstMethod)
-//                .setInsertionPoint(InsertionPoint.class_scope.forType(type))
-//                .build();
-//        File secondFile = firstFile
-//                .toBuilder()
-//                .setContent(secondMethod)
-//                .build();
-//        ImmutableList<CompilerOutput> compilerOutputs = ImmutableList.of(
-//                new TestCompilerOutput(firstFile), new TestCompilerOutput(secondFile)
-//        );
-//        TestGenerator generator = new TestGenerator(compilerOutputs);
-//
-//        CodeGeneratorResponse result = generator.process(request);
-//        assertEquals(1, result.getFileCount());
-//        File file = result.getFile(0);
-//        StringSubject fileContent = Truth.assertThat(file.getContent());
-//        fileContent.contains(firstMethod);
-//        fileContent.contains(secondMethod);
+        SpineProtocConfig config = SpineProtocConfig.getDefaultInstance();
+        CodeGeneratorRequest request = requestBuilder()
+                .addProtoFile(TestGeneratorsProto.getDescriptor()
+                                                 .toProto())
+                .addFileToGenerate(TEST_PROTO_FILE)
+                .setParameter(protocConfig(config, testPluginConfig))
+                .build();
+        MessageType type = new MessageType(EnhancedWithCodeGeneration.getDescriptor());
+        String firstMethod = "public void test1(){}";
+        String secondMethod = "public void test2(){}";
+        File firstFile = File
+                .newBuilder()
+                .setName("file.proto")
+                .setContent(firstMethod)
+                .setInsertionPoint(InsertionPoint.class_scope.forType(type))
+                .build();
+        File secondFile = firstFile
+                .toBuilder()
+                .setContent(secondMethod)
+                .build();
+        ImmutableList<CompilerOutput> compilerOutputs = ImmutableList.of(
+                new TestCompilerOutput(firstFile), new TestCompilerOutput(secondFile)
+        );
+        TestGenerator generator = new TestGenerator(compilerOutputs);
+
+        CodeGeneratorResponse result = generator.process(request);
+        assertThat(result.getFileList())
+                .hasSize(1);
+        File file = result.getFile(0);
+        StringSubject assertFileContent = assertThat(file.getContent());
+        assertFileContent
+                .contains(firstMethod);
+        assertFileContent
+                .contains(secondMethod);
     }
 
     @DisplayName("drop duplicates in generated code for the same insertion point")
     @Test
     void dropCodeDuplicates() {
-//        Methods methods = new Methods();
-//        MessageSelectorFactory messages = methods.messages();
-//        methods.applyFactory(UuidMethodFactory.class.getName(), messages.uuid());
-//        CodeGeneratorRequest request = requestBuilder()
-//                .addProtoFile(TestGeneratorsProto.getDescriptor()
-//                                                 .toProto())
-//                .addFileToGenerate(TEST_PROTO_FILE)
-//                .setParameter(protocConfig(methods, testPluginConfig))
-//                .build();
-//        MessageType type = new MessageType(EnhancedWithCodeGeneration.getDescriptor());
-//        String method = "public void test1(){}";
-//        File generated = File
-//                .newBuilder()
-//                .setName("file.proto")
-//                .setContent(method)
-//                .setInsertionPoint(InsertionPoint.class_scope.forType(type))
-//                .build();
-//        ImmutableList<CompilerOutput> compilerOutputs = ImmutableList.of(
-//                new TestCompilerOutput(generated), new TestCompilerOutput(generated)
-//        );
-//        TestGenerator generator = new TestGenerator(compilerOutputs);
-//
-//        CodeGeneratorResponse result = generator.process(request);
-//        assertEquals(1, result.getFileCount());
-//        File file = result.getFile(0);
-//        StringSubject fileContent = Truth.assertThat(file.getContent());
-//        fileContent.isEqualTo(method);
+        SpineProtocConfig config = SpineProtocConfig.getDefaultInstance();
+        CodeGeneratorRequest request = requestBuilder()
+                .addProtoFile(TestGeneratorsProto.getDescriptor()
+                                                 .toProto())
+                .addFileToGenerate(TEST_PROTO_FILE)
+                .setParameter(protocConfig(config, testPluginConfig))
+                .build();
+        MessageType type = new MessageType(EnhancedWithCodeGeneration.getDescriptor());
+        String method = "public void test1(){}";
+        File generated = File
+                .newBuilder()
+                .setName("file.proto")
+                .setContent(method)
+                .setInsertionPoint(InsertionPoint.class_scope.forType(type))
+                .build();
+        ImmutableList<CompilerOutput> compilerOutputs = ImmutableList.of(
+                new TestCompilerOutput(generated), new TestCompilerOutput(generated)
+        );
+        TestGenerator generator = new TestGenerator(compilerOutputs);
+
+        CodeGeneratorResponse result = generator.process(request);
+        assertThat(result.getFileList())
+                .hasSize(1);
+        File file = result.getFile(0);
+        StringSubject fileContent = assertThat(file.getContent());
+        fileContent
+                .isEqualTo(method);
     }
 
-//    @Nested
-//    @DisplayName("not process invalid `CodeGeneratorRequest` if passed")
-//    class Arguments {
-//
-//        @Test
-//        @DisplayName("`null`")
-//        void nullArg() {
-//            assertNpe(() -> process(null));
-//        }
-//
-//        @Test
-//        @DisplayName("unsupported version")
-//        void notProcessInvalidRequests() {
-//            assertIllegalArgument(() -> process(requestWithUnsupportedVersion()));
-//        }
-//
-//        @Test
-//        @DisplayName("empty request")
-//        void emptyRequest() {
-//            assertIllegalArgument(() -> process(requestBuilder().build()));
-//        }
-//
-//        private void process(CodeGeneratorRequest request) {
-//            new TestGenerator().process(request);
-//        }
-//    }
+    @Nested
+    @DisplayName("not process invalid `CodeGeneratorRequest` if passed")
+    class Arguments {
 
-//    private static CodeGeneratorRequest requestWithUnsupportedVersion() {
-//        CodeGeneratorRequest.Builder result = requestBuilder();
-//        result.setCompilerVersion(result.getCompilerVersionBuilder()
-//                                        .setMajor(2));
-//        return result.build();
-//
-//    }
+        @Test
+        @DisplayName("`null`")
+        void nullArg() {
+            assertNpe(() -> process(null));
+        }
+
+        @Test
+        @DisplayName("unsupported version")
+        void notProcessInvalidRequests() {
+            assertIllegalArgument(() -> process(requestWithUnsupportedVersion()));
+        }
+
+        @Test
+        @DisplayName("empty request")
+        void emptyRequest() {
+            assertIllegalArgument(() -> process(requestBuilder().build()));
+        }
+
+        private void process(CodeGeneratorRequest request) {
+            new TestGenerator().process(request);
+        }
+    }
+
+    private static CodeGeneratorRequest requestWithUnsupportedVersion() {
+        CodeGeneratorRequest.Builder result = requestBuilder();
+        result.setCompilerVersion(result.getCompilerVersionBuilder()
+                                        .setMajor(2));
+        return result.build();
+
+    }
 
     private static class TestGenerator extends CodeGenerator {
 
---
+++ tools/mc-java-protoc/src/test/java/io/spine/tools/mc/java/protoc/given/CodeGeneratorRequestGiven.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2021, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.tools.mc.java.protoc.given;
+
+import com.google.protobuf.DescriptorProtos;
+import com.google.protobuf.compiler.PluginProtos;
+import io.spine.option.OptionsProto;
+import io.spine.tools.protoc.AddInterface;
+import io.spine.tools.protoc.GenerateNestedClasses;
+import io.spine.tools.protoc.MethodFactoryName;
+import io.spine.tools.protoc.NestedClassFactoryName;
+import io.spine.tools.protoc.SpineProtocConfig;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Base64;
+
+import static com.google.common.base.Charsets.UTF_8;
+import static io.spine.tools.mc.java.codegen.Names.className;
+
+/**
+ * A helper class for {@link com.google.protobuf.compiler.PluginProtos.CodeGeneratorRequest
+ * CodeGeneratorRequest}s creation.
+ */
+public final class CodeGeneratorRequestGiven {
+
+    /** Prevents instantiation of this utility class. */
+    private CodeGeneratorRequestGiven() {
+    }
+
+    /**
+     * Creates a {@link com.google.protobuf.compiler.PluginProtos.CodeGeneratorRequest.Builder
+     * CodeGeneratorRequest.Builder} instance with the default Protobuf descriptors and
+     * Spine options set.
+     */
+    public static PluginProtos.CodeGeneratorRequest.Builder requestBuilder() {
+        return PluginProtos.CodeGeneratorRequest
+                .newBuilder()
+                .addProtoFile(descriptorProto())
+                .addProtoFile(spineOptionsProto())
+                .setCompilerVersion(compilerVersion());
+    }
+
+    /**
+     * Creates an instance of the latest supported Protobuf version.
+     */
+    public static PluginProtos.Version compilerVersion() {
+        return PluginProtos.Version.newBuilder()
+                                   .setMajor(3)
+                                   .setMajor(6)
+                                   .setPatch(1)
+                                   .setSuffix("")
+                                   .build();
+    }
+
+    public static GenerateNestedClasses generateNested(Class<?> cls) {
+        return GenerateNestedClasses
+                .newBuilder()
+                .setFactory(nestedClassFactory(cls))
+                .build();
+    }
+
+    private static NestedClassFactoryName nestedClassFactory(Class<?> cls) {
+        return NestedClassFactoryName
+                .newBuilder()
+                .setClassName(className(cls))
+                .build();
+    }
+
+    public static MethodFactoryName methodFactory(Class<?> cls) {
+        return MethodFactoryName
+                .newBuilder()
+                .setClassName(className(cls))
+                .build();
+    }
+
+    public static AddInterface addInterface(Class<?> iface) {
+        return AddInterface
+                .newBuilder()
+                .setName(className(iface))
+                .build();
+    }
+
+    public static String protocConfig(SpineProtocConfig config, Path configPath) {
+        try (FileOutputStream fos = new FileOutputStream(configPath.toFile())) {
+            config.writeTo(fos);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+        return base64Encoded(configPath.toAbsolutePath().toString());
+    }
+
+    private static String base64Encoded(String value) {
+        byte[] valueBytes = value.getBytes(UTF_8);
+        String result = Base64.getEncoder()
+                              .encodeToString(valueBytes);
+        return result;
+    }
+
+    private static DescriptorProtos.FileDescriptorProto spineOptionsProto() {
+        return OptionsProto.getDescriptor()
+                           .toProto();
+    }
+
+    private static DescriptorProtos.FileDescriptorProto descriptorProto() {
+        return DescriptorProtos.getDescriptor()
+                               .toProto();
+    }
+}
