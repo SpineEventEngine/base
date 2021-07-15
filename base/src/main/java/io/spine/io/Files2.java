355f28ed2f0c627c01806f353d368dc92264bb0c
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-07-16 00:32:46 +0300

    Optimise imports

--- base/src/main/java/io/spine/io/Files2.java
+++ base/src/main/java/io/spine/io/Files2.java
@@ -31,9 +31,7 @@ import com.google.errorprone.annotations.CanIgnoreReturnValue;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
