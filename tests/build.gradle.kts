00625e65d8e1f96ca4d65e42bb952662f966dca6
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-07-24 00:54:15 +0300

    Ignore duplicates for a while

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -139,6 +139,10 @@ subprojects {
 
         include("**/*Test.class")
     }
+
+    //TODO:2021-07-22:alexander.yevsyukov: Turn to WARN and investigate duplicates.
+    // see https://github.com/SpineEventEngine/base/issues/657
+    tasks.processTestResources.get().duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
 
 val scriptsPath = Scripts.commonPath
