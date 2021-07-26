a66b22276c61ebf01ccafdaa3d6d20b9d37be658
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2021-07-26 21:25:18 +0300

    Group tests

--- license-report.md
+++ license-report.md
@@ -431,7 +431,7 @@
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:05 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:27 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -911,7 +911,7 @@ This report was generated on **Mon Jul 26 17:41:05 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:08 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:30 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -1383,7 +1383,7 @@ This report was generated on **Mon Jul 26 17:41:08 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:09 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:31 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -1855,7 +1855,7 @@ This report was generated on **Mon Jul 26 17:41:09 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:10 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:32 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -2343,7 +2343,7 @@ This report was generated on **Mon Jul 26 17:41:10 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:11 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:33 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -2887,7 +2887,7 @@ This report was generated on **Mon Jul 26 17:41:11 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:12 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:33 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -3367,7 +3367,7 @@ This report was generated on **Mon Jul 26 17:41:12 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:12 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:33 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -3807,7 +3807,7 @@ This report was generated on **Mon Jul 26 17:41:12 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:13 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:35 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -4279,7 +4279,7 @@ This report was generated on **Mon Jul 26 17:41:13 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:15 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:36 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -4766,7 +4766,7 @@ This report was generated on **Mon Jul 26 17:41:15 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:16 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:37 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -5238,7 +5238,7 @@ This report was generated on **Mon Jul 26 17:41:16 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:17 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:38 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -5765,7 +5765,7 @@ This report was generated on **Mon Jul 26 17:41:17 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:18 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:39 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -6252,7 +6252,7 @@ This report was generated on **Mon Jul 26 17:41:18 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:20 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:40 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
 
 
 
@@ -6692,4 +6692,4 @@ This report was generated on **Mon Jul 26 17:41:20 EEST 2021** using [Gradle-Lic
  The dependencies distributed under several licenses, are used according their commercial-use-friendly license.
 
 
-This report was generated on **Mon Jul 26 17:41:20 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
+This report was generated on **Mon Jul 26 20:45:40 EEST 2021** using [Gradle-License-Report plugin](https://github.com/jk1/Gradle-License-Report) by Evgeny Naumenko, licensed under [Apache 2.0 License](https://github.com/jk1/Gradle-License-Report/blob/master/LICENSE).
--- tests/model-compiler/src/test/java/io/spine/tools/protoc/ProtocPluginTest.java
+++ tests/model-compiler/src/test/java/io/spine/tools/protoc/ProtocPluginTest.java
@@ -51,15 +51,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Method;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SuppressWarnings("InnerClassMayBeStatic")
 @DisplayName("`ProtocPlugin` should")
@@ -96,25 +93,46 @@ final class ProtocPluginTest {
     @Test
     @DisplayName("implement interface in the generated messages with `IS` option")
     void implementInterfaceInGeneratedMessagesWithIsOption() {
-        assertThat(PICustomerCreated.getDefaultInstance()).isInstanceOf(PICustomerEvent.class);
-        assertThat(PICreateCustomer.getDefaultInstance()).isInstanceOf(PICustomerCommand.class);
+        PICustomerCreated event = PICustomerCreated.getDefaultInstance();
+        assertThat(event).isInstanceOf(PICustomerEvent.class);
+        
+        PICreateCustomer cmd = PICreateCustomer.getDefaultInstance();
+        assertThat(cmd).isInstanceOf(PICustomerCommand.class);
     }
 
     @Test
     @DisplayName("use `IS` and `EVERY IS` together")
-    void useIsInPriorityToEveryIs() {
-        assertThat(PIUserCreated.getDefaultInstance()).isInstanceOf(PIUserEvent.class);
-        assertThat(PIUserNameUpdated.getDefaultInstance()).isInstanceOf(PIUserEvent.class);
+    void isAndEveryIsTogether() {
+        assertThat(PIUserCreated.getDefaultInstance())
+                .isInstanceOf(PIUserEvent.class);
+        assertThat(PIUserNameUpdated.getDefaultInstance())
+                .isInstanceOf(PIUserEvent.class);
 
-        assertThat(UserName.getDefaultInstance()).isInstanceOf(PIUserEvent.class);
-        assertThat(UserName.getDefaultInstance()).isInstanceOf(UserInfo.class);
+        assertThat(UserName.getDefaultInstance())
+                /*
+                   This assertion verifies if the `UserName` implements a custom
+                   event interface. It happens because `UserName` is declared in the same file with
+                   events (see `mixed_test.proto`) and the following option is applied:
+
+                   option (every_is).java_type = "io.spine.tools.protoc.test.PIUserEvent";
+
+                   This test should be updated to avoid the confusion on why a value object
+                   becomes an event. Although, it serves for the purposes of the test, it
+                   is misleading because a normal production code shouldn't have
+                   such an arrangement.
+                */
+                .isInstanceOf(PIUserEvent.class);
+
+        assertThat(UserName.getDefaultInstance())
+                .isInstanceOf(UserInfo.class);
     }
 
     @Test
     @DisplayName("resolve packages from src proto if the packages are not specified")
-    void resolvePackagesFromSrcProtoIfNotSpecified() throws ClassNotFoundException {
+    void resolvingPackages() throws ClassNotFoundException {
         Class<?> cls = checkMarkerInterface(USER_COMMAND_FQN);
-        assertTrue(cls.isAssignableFrom(PICreateUser.class));
+        assertThat(PICreateUser.class)
+                .isAssignableTo(cls);
     }
 
     @Test
@@ -128,71 +146,116 @@ final class ProtocPluginTest {
         );
     }
 
-    @Test
-    @DisplayName("mark as event messages")
-    void skipStandardTypesIfIgnored() {
-        assertThat(UserCreated.getDefaultInstance()).isInstanceOf(EventMessage.class);
-        assertThat(UserNotified.getDefaultInstance()).isInstanceOf(EventMessage.class);
+    @Nested
+    @DisplayName("mark")
+    class MarkingWithInterfaces {
 
-        assertThat(TypicalIdentifier.getDefaultInstance()).isInstanceOf(UuidValue.class);
-    }
+        @Test
+        @DisplayName("command messages")
+        void markCommandMessages() {
+            assertThat(CreateUser.getDefaultInstance())
+                    .isInstanceOf(CommandMessage.class);
+            assertThat(NotifyUser.getDefaultInstance())
+                    .isInstanceOf(CommandMessage.class);
+        }
 
-    @Test
-    @DisplayName("mark as command messages")
-    void markCommandMessages() {
-        assertThat(CreateUser.getDefaultInstance()).isInstanceOf(CommandMessage.class);
-        assertThat(NotifyUser.getDefaultInstance()).isInstanceOf(CommandMessage.class);
-    }
+        @Test
+        @DisplayName("event messages")
+        void markMessages() {
+            assertThat(UserCreated.getDefaultInstance())
+                    .isInstanceOf(EventMessage.class);
+            assertThat(UserNotified.getDefaultInstance())
+                    .isInstanceOf(EventMessage.class);
+        }
 
-    @Test
-    @DisplayName("mark as rejection messages")
-    void markRejectionMessages() {
-        assertThat(Rejections.UserAlreadyExists.getDefaultInstance())
-                .isInstanceOf(RejectionMessage.class);
-        assertThat(Rejections.UserAlreadyExists.getDefaultInstance())
-                .isInstanceOf(UserRejection.class);
-    }
+        @Test
+        @DisplayName("rejection messages")
+        void markRejectionMessages() {
+            assertThat(Rejections.UserAlreadyExists.getDefaultInstance())
+                    .isInstanceOf(RejectionMessage.class);
+            assertThat(Rejections.UserAlreadyExists.getDefaultInstance())
+                    .isInstanceOf(UserRejection.class);
+        }
 
-    @Test
-    @DisplayName("mark messages with already existing interface types")
-    @SuppressWarnings("UnnecessaryLocalVariable")
-        // Compile-time verification.
-    void implementHandcraftedInterfaces() {
-        assertThat(Rejections.UserAlreadyExists.getDefaultInstance())
-                .isInstanceOf(UserRejection.class);
-        assertFalse(Message.class.isAssignableFrom(UserRejection.class));
-        String id = Identifier.newUuid();
-        Rejections.UserAlreadyExists message = Rejections.UserAlreadyExists
-                .newBuilder()
-                .setId(id)
-                .build();
-        UserRejection rejection = message;
-        assertEquals(id, rejection.getId());
-    }
+        @Test
+        @DisplayName("mark UUID-based identifiers")
+        void markUuids() {
+            assertThat(TypicalIdentifier.getDefaultInstance())
+                    .isInstanceOf(UuidValue.class);
+        }
 
-    @Test
-    @DisplayName("mark nested message declarations by `(is)` option")
-    void markNestedTypes() {
-        assertThat(Outer.Inner.class).isAssignableTo(Wrapped.class);
-    }
+        @Test
+        @DisplayName("messages with already existing interface types")
+        @SuppressWarnings("UnnecessaryLocalVariable")
+            // Compile-time verification.
+        void implementHandcraftedInterfaces() {
+            assertThat(Rejections.UserAlreadyExists.getDefaultInstance())
+                    .isInstanceOf(UserRejection.class);
+            assertFalse(Message.class.isAssignableFrom(UserRejection.class));
 
-    @Test
-    @DisplayName("mark nested message declarations by `(every_is)` option")
-    void markEveryNested() {
-        assertThat(Kindergarten.class).isAssignableTo(EducationalInstitution.class);
-        assertThat(School.class).isAssignableTo(EducationalInstitution.class);
-        assertThat(School.Elementary.class).isAssignableTo(EducationalInstitution.class);
-        assertThat(School.HighSchool.class).isAssignableTo(EducationalInstitution.class);
-        assertThat(University.class).isAssignableTo(EducationalInstitution.class);
-        assertThat(University.College.class).isAssignableTo(EducationalInstitution.class);
-    }
+            String id = Identifier.newUuid();
+            Rejections.UserAlreadyExists message = Rejections.UserAlreadyExists.newBuilder()
+                    .setId(id)
+                    .build();
+            UserRejection rejection = message;
+            assertEquals(id, rejection.getId());
+        }
 
-    @Test
-    @DisplayName("mark top-level message declarations as specified in `modelCompiler` settings")
-    void markMessagesByFilePattern() {
-        assertThat(WeatherForecast.class).isAssignableTo(DocumentMessage.class);
-        assertThat(WeatherForecast.Temperature.getDefaultInstance())
-                .isNotInstanceOf(DocumentMessage.class);
+        @Nested
+        @DisplayName("nested message declarations by")
+        class NestedMessages {
+
+            @Test
+            @DisplayName("`(is)` option")
+            void markNestedTypes() {
+                assertThat(Outer.Inner.class).isAssignableTo(Wrapped.class);
+            }
+
+            @Test
+            @DisplayName("`(every_is)` option")
+            void markEveryNested() {
+                assertThat(Kindergarten.class).isAssignableTo(EducationalInstitution.class);
+                assertThat(School.class).isAssignableTo(EducationalInstitution.class);
+                assertThat(School.Elementary.class).isAssignableTo(EducationalInstitution.class);
+                assertThat(School.HighSchool.class).isAssignableTo(EducationalInstitution.class);
+                assertThat(University.class).isAssignableTo(EducationalInstitution.class);
+                assertThat(University.College.class).isAssignableTo(EducationalInstitution.class);
+            }
+        }
+
+        @Test
+        @DisplayName("top-level message declarations as specified in `modelCompiler` settings")
+        void markMessagesByFilePattern() {
+            assertThat(WeatherForecast.class).isAssignableTo(DocumentMessage.class);
+            assertThat(WeatherForecast.Temperature.getDefaultInstance())
+                    .isNotInstanceOf(DocumentMessage.class);
+        }
+
+        @Nested
+        @DisplayName("a message with the interface using")
+        final class MarkMessages {
+
+            @Test
+            @DisplayName("regex pattern")
+            void regex() {
+                assertThat(MessageEnhancedWithRegexGenerations.getDefaultInstance())
+                        .isInstanceOf(RegexedMessage.class);
+            }
+
+            @Test
+            @DisplayName("prefix pattern")
+            void prefix() {
+                assertThat(MessageEnhancedWithPrefixGenerations.getDefaultInstance())
+                        .isInstanceOf(PrefixedMessage.class);
+            }
+
+            @Test
+            @DisplayName("suffix pattern")
+            void postfix() {
+                assertThat(MessageEnhancedWithSuffixGenerations.getDefaultInstance())
+                        .isInstanceOf(SuffixedMessage.class);
+            }
+        }
     }
 
     @Test
@@ -201,13 +264,6 @@ final class ProtocPluginTest {
         MessageType expectedType =
                 new MessageType(MessageEnhancedWithSuffixGenerations.getDescriptor());
         assertEquals(expectedType, MessageEnhancedWithSuffixGenerations.ownType());
-    }
-
-    @Test
-    @DisplayName("mark a message with interface using `.suffix()` pattern")
-    void markMessageWithInterfaceUsingEndsWithPattern() {
-        assertThat(MessageEnhancedWithSuffixGenerations.getDefaultInstance())
-                .isInstanceOf(SuffixedMessage.class);
     }
 
     @Test
@@ -223,31 +279,6 @@ final class ProtocPluginTest {
         assertEquals(TypicalIdentifier.of(uuid), TypicalIdentifier.of(uuid));
     }
 
-    @Nested
-    @DisplayName("mark a message with the interface using")
-    final class MarkMessages {
-
-        @Test
-        @DisplayName("regex pattern")
-        void regex() {
-            assertThat(MessageEnhancedWithRegexGenerations.getDefaultInstance())
-                    .isInstanceOf(RegexedMessage.class);
-        }
-
-        @Test
-        @DisplayName("prefix pattern")
-        void prefix() {
-            assertThat(MessageEnhancedWithPrefixGenerations.getDefaultInstance())
-                    .isInstanceOf(PrefixedMessage.class);
-        }
-
-        @Test
-        @DisplayName("suffix pattern")
-        void postfix() {
-            assertThat(MessageEnhancedWithSuffixGenerations.getDefaultInstance())
-                    .isInstanceOf(SuffixedMessage.class);
-        }
-    }
 
     @Nested
     @DisplayName("generate a custom method for a message using")
@@ -353,8 +384,8 @@ final class ProtocPluginTest {
         Class<?> cls = Class.forName(fqn);
         assertThat(cls.isInterface())
                 .isTrue();
-        assertThat(Message.class)
-                .isAssignableTo(cls);
+        assertThat(cls)
+                .isAssignableTo(Message.class);
 
         assertThat(cls.getDeclaredMethods())
                 .hasLength(0);
