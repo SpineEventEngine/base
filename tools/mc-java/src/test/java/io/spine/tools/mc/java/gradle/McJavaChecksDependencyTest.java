6c247dfc80885a287a68f89f4c608f0fbe052854
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2021-08-02 14:25:36 +0300

    Fix disabled test annotation

--- tools/mc-java/src/test/java/io/spine/tools/mc/java/gradle/McJavaChecksDependencyTest.java
+++ tools/mc-java/src/test/java/io/spine/tools/mc/java/gradle/McJavaChecksDependencyTest.java
@@ -32,8 +32,8 @@ import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.artifacts.DependencySet;
-import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -61,7 +61,7 @@ class McJavaChecksDependencyTest {
     }
 
     @Test
-    @Ignore("Disable temporarily until the mystery with missing transitive depenencies is solved")
+    @Disabled("Disable temporarily until the mystery with missing transitive depenencies is solved")
     @DisplayName("add Spine Java Checks dependency to annotation processor config of a project")
     void addToProject() {
         Project project = stubProject.withMavenRepositories().get();
