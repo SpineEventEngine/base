fa1323043a121aa70763681faf6a9f9fcef26f57
Alex Tymchenko <alex.tymchenko@teamdev.com>
2021-12-09 19:57:19 +0200

    Bump the version and update the license report files.

--- version.gradle.kts
+++ version.gradle.kts
@@ -38,7 +38,7 @@
  */
 
 /** The version of this library. */
-val base = "2.0.0-SNAPSHOT.78"
+val base = "2.0.0-SNAPSHOT.79"
 
 val spineVersion: String by extra(base)
 val spineBaseVersion: String by extra(base) // Used by `filter-internal-javadoc.gradle`.
