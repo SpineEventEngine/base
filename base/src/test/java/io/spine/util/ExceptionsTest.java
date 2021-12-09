b54b0996a6ed4fce919fccc17cc1dcc6ebec6c9e
Alex Tymchenko <alex.tymchenko@teamdev.com>
2021-12-09 18:48:34 +0200

    Use `var`s in `io.spine.util` test package.

--- base/src/test/java/io/spine/util/ExceptionsTest.java
+++ base/src/test/java/io/spine/util/ExceptionsTest.java
@@ -41,7 +41,7 @@ import static io.spine.util.Exceptions.unsupported;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DisplayName("Exceptions utility class should")
+@DisplayName("`Exceptions` utility class should")
 class ExceptionsTest extends UtilityClassTest<Exceptions> {
 
     ExceptionsTest() {
@@ -78,13 +78,12 @@ class ExceptionsTest extends UtilityClassTest<Exceptions> {
         @Test
         @DisplayName("with formatted message")
         void formattedMessage() {
-            String arg1 = getClass().getCanonicalName();
-            long arg2 = 100500L;
-            UnsupportedOperationException exception =
-                    assertThrows(
-                            UnsupportedOperationException.class,
-                            () -> unsupported("%s %d", arg1, arg2));
-            String exceptionMessage = exception.getMessage();
+            var arg1 = getClass().getCanonicalName();
+            var arg2 = 100500L;
+            var exception = assertThrows(
+                    UnsupportedOperationException.class,
+                    () -> unsupported("%s %d", arg1, arg2));
+            var exceptionMessage = exception.getMessage();
             assertTrue(exceptionMessage.contains(arg1));
             assertTrue(exceptionMessage.contains(String.valueOf(arg2)));
         }
@@ -112,7 +111,7 @@ class ExceptionsTest extends UtilityClassTest<Exceptions> {
     }
 
     @Nested
-    @DisplayName("throw IllegalStateException with")
+    @DisplayName("throw `IllegalStateException` with")
     @SuppressWarnings({"ResultOfMethodCallIgnored", "ThrowableNotThrown"})
     class ThrowISE {
 
