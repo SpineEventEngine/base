767cce70f29ff5f7dbe98cfd8f1a5032515084b4
yevhenii.nadtochii <yevhenii.nadtochii@teamdev.com>
2022-03-22 18:45:20 +0200

    Bump the version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -32,5 +33,7 @@ pluginManagement {
 
 rootProject.name = "spine-base"
 
-include("base")
-include("testlib")
+include(
+    "base",
+    "testlib",
+)
