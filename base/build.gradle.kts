ce3f216fc17bb98baff998e9c97b8cf687ac7ab6
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-04-23 20:58:43 +0300

    Extract proto task config. fun

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -24,6 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import com.google.protobuf.gradle.GenerateProtoTask
 import com.google.protobuf.gradle.generateProtoTasks
 import com.google.protobuf.gradle.protobuf
 import io.spine.internal.dependency.AutoService
@@ -51,69 +52,75 @@ dependencies {
 
 val generatedDir by extra("$projectDir/generated")
 
-sourceSets {
-    val generatedSpineDir by extra("$generatedDir/main/java")
-    val generatedTestSpineDir by extra("$generatedDir/test/java")
-
-    main {
-        java.srcDir(generatedSpineDir)
-        resources.srcDir("$buildDir/descriptors/main")
-    }
-    test {
-        java.srcDir(generatedTestSpineDir)
-        resources.srcDir("$buildDir/descriptors/test")
-    }
-}
-
 protobuf {
     configurations.excludeProtobufLite()
     generatedFilesBaseDir = generatedDir
 
     generateProtoTasks {
         for (task in all()) {
-            val ssn = task.sourceSet.name
-            task.generateDescriptorSet = true
-            with(task.descriptorSetOptions) {
-                path = "$buildDir/descriptors/${ssn}/known_types_${ssn}.desc"
-                includeImports = true
-                includeSourceInfo = true
-            }
-
-            /**
-             * Remove the code generated for Google Protobuf library types.
-             *
-             * Java code for the `com.google` package was generated because we wanted
-             * to have descriptors for all the types, including those from Google Protobuf library.
-             * We want all the descriptors so that they are included into the resources used by
-             * the `io.spine.type.KnownTypes` class.
-             *
-             * Now, as we have the descriptors _and_ excessive Java code, we delete it to avoid
-             * classes that duplicate those coming from Protobuf library JARs.
-             */
-            task.doLast {
-                val comPackage = File("${generatedDir}/${ssn}/java/com")
-                val googlePackage = comPackage.resolve("google")
-
-                delete(googlePackage)
-
-                // We don't need an empty `com` package.
-                if (comPackage.exists() && comPackage.list()?.isEmpty() == true) {
-                    delete(comPackage)
-                }
-            }
-
-            /*
-             * Make the tasks `processResources` depend on `generateProto` tasks explicitly so that:
-             *  1) descriptor set files get into resources, avoiding the racing conditions
-             *     during the build.
-             *  2) we don't have the warning "Execution optimizations have been disabled..." issued
-             *     by Gradle during the build because Protobuf Gradle Plugin does not set
-             *     dependencies between `generateProto` and `processResources` tasks.
-             */
-            val processResources = processResourceTaskName(ssn)
-            tasks[processResources].dependsOn(task)
+            configureProtoTask(task)
         }
     }
+}
+
+/**
+ * Configures protobuf code generation task.
+ *
+ * The task configuration consists of the following steps.
+ *
+ * 1. Generation of descriptor set file is turned op for each source set.
+ *    These files are placed under the `build/descriptors` directory.
+ *
+ * 2. At the final steps of the code generation, the code belonging to the `com.google` package
+ *    is removed.
+ *
+ * 3. Make `processResource` tasks depend on corresponding `generateProto` tasks.
+ */
+fun configureProtoTask(task: GenerateProtoTask) {
+    /**
+     * Generate descriptor set files.
+     */
+    val ssn = task.sourceSet.name
+    task.generateDescriptorSet = true
+    with(task.descriptorSetOptions) {
+        path = "$buildDir/descriptors/${ssn}/known_types_${ssn}.desc"
+        includeImports = true
+        includeSourceInfo = true
+    }
+
+    /**
+     * Remove the code generated for Google Protobuf library types.
+     *
+     * Java code for the `com.google` package was generated because we wanted
+     * to have descriptors for all the types, including those from Google Protobuf library.
+     * We want all the descriptors so that they are included into the resources used by
+     * the `io.spine.type.KnownTypes` class.
+     *
+     * Now, as we have the descriptors _and_ excessive Java code, we delete it to avoid
+     * classes that duplicate those coming from Protobuf library JARs.
+     */
+    task.doLast {
+        val comPackage = File("${generatedDir}/${ssn}/java/com")
+        val googlePackage = comPackage.resolve("google")
+
+        delete(googlePackage)
+
+        // We don't need an empty `com` package.
+        if (comPackage.exists() && comPackage.list()?.isEmpty() == true) {
+            delete(comPackage)
+        }
+    }
+
+    /**
+     * Make the tasks `processResources` depend on `generateProto` tasks explicitly so that:
+     *  1) descriptor set files get into resources, avoiding the racing conditions
+     *     during the build.
+     *  2) we don't have the warning "Execution optimizations have been disabled..." issued
+     *     by Gradle during the build because Protobuf Gradle Plugin does not set
+     *     dependencies between `generateProto` and `processResources` tasks.
+     */
+    val processResources = processResourceTaskName(ssn)
+    tasks[processResources].dependsOn(task)
 }
 
 fun processResourceTaskName(sourceSetName: String): String {
