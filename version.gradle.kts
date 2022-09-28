8ff405809b05913c2dac9f264c7bec45cfb9d943
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-09-28 19:42:17 +0300

    Bump `base` -> `2.0.0-SNAPSHOT.102`

--- version.gradle.kts
+++ version.gradle.kts
@@ -38,7 +38,7 @@
  */
 
 /** The version of this library. */
-val base = "2.0.0-SNAPSHOT.101"
+val base = "2.0.0-SNAPSHOT.102"
 
 val spineVersion: String by extra(base)
 val spineBaseVersion: String by extra(base) // Used by `filter-internal-javadoc.gradle`.
