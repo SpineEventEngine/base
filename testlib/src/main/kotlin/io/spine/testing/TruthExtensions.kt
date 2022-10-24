3d94655ea4f5031ccb25644ae5961e73645b06db
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-10-24 15:57:55 +0300

    Add extension fun for `Subject`s

--- testlib/src/main/kotlin/io/spine/testing/TruthExtensions.kt
+++ testlib/src/main/kotlin/io/spine/testing/TruthExtensions.kt
@@ -27,6 +27,29 @@
 package io.spine.testing
 
 import com.google.common.testing.NullPointerTester
+import com.google.common.truth.IterableSubject
+import com.google.common.truth.OptionalSubject
+import com.google.common.truth.StringSubject
+import com.google.common.truth.StringSubject.CaseInsensitiveStringComparison
+import com.google.common.truth.Truth
+import com.google.common.truth.Truth8
+import java.util.*
+
+/**
+ * Extension functions for [Google Truth](https://truth.dev/).
+ *
+ * It is expected that these extensions would be implemented by Truth someday.
+ * Until then, we would use those implemented below.
+ *
+ * @see <a href="https://github.com/google/truth/milestone/12">
+ *     "First post-1.0 feature push" milestone at Truth GitHub</a>
+ * @see <a href="https://github.com/google/truth/issues/536">
+ *     Issue in Truth GitHub</a>
+ * @see <a href="https://github.com/google/truth/issues/572">
+ *     Another issue</a>
+ */
+@Suppress("unused") // is used for KDoc on this file.
+private const val ABOUT = ""
 
 /**
  * Allows to use generic parameter of the function instead of `MyType::class.java` as the first
@@ -34,3 +57,59 @@ import com.google.common.testing.NullPointerTester
  */
 public inline fun <reified T : Any> NullPointerTester.setDefault(value : T) : NullPointerTester =
     setDefault(T::class.java, value)
+
+/**
+ * Allows to write:
+ *
+ * ```kotlin
+ *  assertThat("foo bar") {
+ *      contains("foo")
+ *      contains("bar")
+ *  }
+ * ```
+ */
+public fun assertThat(string: String, assertions: StringSubject.() -> Unit) {
+    Truth.assertThat(string).run { assertions() }
+}
+
+/**
+ * Allows to write:
+ *
+ * ```kotlin
+ * assertThat("foo bar").ignoringCase() {
+ *    contains("BAR")
+ *    contains("FoO")
+ * }
+ * ```
+ */
+public fun StringSubject.ignoringCase(assertions: CaseInsensitiveStringComparison.() -> Unit) {
+    assertions(ignoringCase())
+}
+
+/**
+ * Allows to write:
+ *
+ * ```kotlin
+ * assertThat(listOf(1, 2, 3) {
+ *     contains(1)
+ *     contains(3)
+ *     doesNotContain(4)
+ * }
+ * ```
+ */
+public fun <T> assertThat(iterable: Iterable<T>, assertions: IterableSubject.() -> Unit) {
+    Truth.assertThat(iterable).run { assertions() }
+}
+
+/**
+ * Allows to write:
+ *
+ * ```kotlin
+ * assertThat(Optional.of("something") {
+ *     hasValue("something")
+ * }
+ * ```
+ */
+public fun <T> assertThat(optional: Optional<T>, assertions: OptionalSubject.() -> Unit) {
+    Truth8.assertThat(optional).run { assertions() }
+}
