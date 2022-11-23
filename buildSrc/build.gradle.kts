b9cf5aa0b1bb287d2a985698534eecdfbb8bafe3
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-11-23 23:05:10 +0300

    Improve constant name

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -80,7 +80,7 @@ val guavaVersion = "31.1-jre"
  * @see <a href="https://github.com/tbroyer/gradle-errorprone-plugin/releases">
  *     Error Prone Gradle Plugin Releases</a>
  */
-val errorProneVersion = "3.0.1"
+val errorPronePluginVersion = "3.0.1"
 
 /**
  * The version of Protobuf Gradle Plugin.
@@ -143,7 +143,7 @@ dependencies {
     implementation("com.google.guava:guava:$guavaVersion")
     api("com.github.jk1:gradle-license-report:$licenseReportVersion")
     implementation("org.ajoberstar.grgit:grgit-core:${grGitVersion}")
-    implementation("net.ltgt.gradle:gradle-errorprone-plugin:${errorProneVersion}")
+    implementation("net.ltgt.gradle:gradle-errorprone-plugin:${errorPronePluginVersion}")
 
     // Add explicit dependency to avoid warning on different Kotlin runtime versions.
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
