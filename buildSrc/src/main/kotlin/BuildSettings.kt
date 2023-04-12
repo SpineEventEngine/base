c45aa300abd3671ba4a314fae8541a9bbe0f0f7b
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-04-12 19:55:46 +0100

    Document build settings object

--- buildSrc/src/main/kotlin/BuildSettings.kt
+++ buildSrc/src/main/kotlin/BuildSettings.kt
@@ -26,6 +26,10 @@
 
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 
+/**
+ * This object provides high-level constants, like version of JVM, to be used
+ * throughout the project.
+ */
 object BuildSettings {
     private const val JVM_VERSION = 11
     val javaVersion: JavaLanguageVersion = JavaLanguageVersion.of(JVM_VERSION)
