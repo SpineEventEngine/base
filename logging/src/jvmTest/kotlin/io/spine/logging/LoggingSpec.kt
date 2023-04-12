87b704e0422ac05ec2a74169a2ab6ee74966978d
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-04-12 14:52:41 +0100

    Consolidate tests of `Logging`

--- logging/src/jvmTest/kotlin/io/spine/logging/LoggingSpec.kt
+++ logging/src/jvmTest/kotlin/io/spine/logging/LoggingSpec.kt
@@ -26,28 +26,88 @@
 
 package io.spine.logging
 
+import com.google.common.flogger.FluentLogger
+import com.google.common.flogger.LogContext
+import com.google.common.flogger.backend.LogData
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.types.shouldBeSameInstanceAs
+import io.spine.logging.given.LoggingObject
+import io.spine.testing.logging.LogTruth
+import java.util.function.Supplier
+import java.util.logging.Level
+import java.util.logging.Logger
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 @DisplayName("`Logging` interface should")
 internal class LoggingSpec {
 
     @Test
-    fun `provide the same logger instance`() {
-        val im = ImplementsLogging()
-
-        im.logger() shouldBeSameInstanceAs im.logger()
+    fun `assume 'FINE' for debug`() {
+        Logging.debugLevel() shouldBe Level.FINE
     }
-}
 
-private class ImplementsLogging: Logging {
-
-    fun showLogging() {
-        _info().log("Here comes info from `$javaClass`.")
+    @Test
+    fun `assume 'SEVER' for errors`() {
+        Logging.errorLevel() shouldBe Level.SEVERE
     }
-}
 
-fun main() {
-    ImplementsLogging().showLogging()
+    @Test
+    fun `obtain same 'FluentLogger' Instance`() {
+        val obj: Logging = LoggingObject()
+        val logger = obj.logger()
+
+        obj.logger() shouldBeSameInstanceAs logger
+        LoggingObject().logger() shouldBeSameInstanceAs logger
+    }
+
+    @Test
+    fun `provide a static API to obtain a logger for a class`() {
+        val logger = Logging.loggerFor(LoggingObject::class.java)
+        logger shouldNotBe null
+        logger shouldBeSameInstanceAs LoggingObject().logger()
+    }
+
+    @Nested
+    @DisplayName("expose shortcut methods")
+    internal inner class Shortcuts {
+
+        private lateinit var obj: Logging
+        private lateinit var julLogger: Logger
+
+        @BeforeEach
+        fun createLoggingObject() {
+            obj = LoggingObject()
+            julLogger = Logger.getLogger(obj.javaClass.name)
+        }
+
+        @Test
+        fun `for standard logging levels`() {
+            assertApi({ obj._config() }, Level.CONFIG)
+            assertApi({ obj._fine() }, Level.FINE)
+            assertApi({ obj._finer() }, Level.FINER)
+            assertApi({ obj._finest() }, Level.FINEST)
+            assertApi({ obj._info() }, Level.INFO)
+            assertApi({ obj._severe() }, Level.SEVERE)
+            assertApi({ obj._warn() }, Level.WARNING)
+        }
+
+        @Test
+        fun `for popular aliases`() {
+            assertApi({ obj._debug() }, Level.FINE)
+            assertApi({ obj._trace() }, Level.FINEST)
+            assertApi({ obj._error() }, Level.SEVERE)
+        }
+
+        private fun assertApi(method: Supplier<FluentLogger.Api>, expectedLevel: Level) {
+            julLogger.level = expectedLevel
+            val api// See: https://github.com/SpineEventEngine/base/issues/612
+                    = method.get()
+            LogTruth.assertThat(api).isInstanceOf(LogContext::class.java)
+            (api as LogData).level shouldBe expectedLevel
+        }
+    }
 }
