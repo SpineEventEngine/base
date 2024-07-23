c954109275c444a16a9071009e579210fe06ff5a
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-07-23 02:39:14 +0100

    Bump version -> `2.0.0-SNAPSHOT.204`

--- version.gradle.kts
+++ version.gradle.kts
@@ -24,4 +24,4 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-val versionToPublish: String by extra("2.0.0-SNAPSHOT.203")
+val versionToPublish: String by extra("2.0.0-SNAPSHOT.204")
