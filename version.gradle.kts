294030d0809a8d05fe825829b50361e13934d6bc
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-02-05 16:54:16 +0000

    Update config

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,4 +24,4 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-val versionToPublish: String by extra("2.0.0-SNAPSHOT.237")
+val versionToPublish: String by extra("2.0.0-SNAPSHOT.238")
