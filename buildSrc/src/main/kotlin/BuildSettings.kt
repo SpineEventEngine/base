851c59134cd314b13743758ae7f33ef21ae21c35
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-03-14 13:05:42 +0000

    Suppress `unused`

--- buildSrc/src/main/kotlin/BuildSettings.kt
+++ buildSrc/src/main/kotlin/BuildSettings.kt
@@ -32,6 +32,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
  * This object provides high-level constants, like the version of JVM, to be used
  * throughout the project.
  */
+@Suppress("unused")
 object BuildSettings {
     private const val JVM_VERSION = 17
     val javaVersion: JavaLanguageVersion = JavaLanguageVersion.of(JVM_VERSION)
