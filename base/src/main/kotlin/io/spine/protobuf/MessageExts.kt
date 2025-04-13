9aab42e3ff75448aa1e4cb46e92cb630575e5328
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-04-13 21:26:21 +0100

    Use `ClassValue` for calculating default message instances

--- base/src/main/kotlin/io/spine/protobuf/MessageExts.kt
+++ base/src/main/kotlin/io/spine/protobuf/MessageExts.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2023, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -30,9 +30,9 @@ package io.spine.protobuf
 
 import com.google.common.cache.CacheBuilder
 import com.google.common.cache.CacheLoader
-import com.google.common.util.concurrent.UncheckedExecutionException
 import com.google.protobuf.Any
 import com.google.protobuf.Message
+import io.spine.annotation.Experimental
 import io.spine.annotation.Internal
 import java.lang.reflect.Type
 import kotlin.reflect.KClass
@@ -46,14 +46,39 @@ import kotlin.reflect.KClass
 public val <M : Message> Class<M>.defaultInstance: M
     get() {
         @Suppress("UNCHECKED_CAST")
-        return defaultInstances.getUnchecked(this) as M
+        return DefaultInstanceValue.get(this) as M
+        // The previously used approach is commented out below.
+        // Please do not remove until the `DefaultInstanceValue` object
+        // goes out of the experimental status.
+        //return defaultInstances.getUnchecked(this) as M
     }
+
+/**
+ * Associates default message instances with their classes using the [ClassValue] API.
+ *
+ * This is an experimental API, which aims to replace the [defaultInstances] cache.
+ */
+@Experimental
+private object DefaultInstanceValue : ClassValue<Message>() {
+
+    /**
+     * Obtains a default instance for a message class.
+     */
+    override fun computeValue(type: Class<*>): Message {
+        @Suppress("UNCHECKED_CAST") // It's OK: we control this private API.
+        val messageType = type as Class<out Message>
+        // It is safe to use the `Internal` utility class from Protobuf since it relies on
+        // the fact that the generated class has the `getDefaultInstance()` static method.
+        return com.google.protobuf.Internal.getDefaultInstance(messageType)
+    }
+}
 
 /**
  * The cache of the default instances per [Message] class.
  *
  * Creates and caches objects in a lazy mode.
  */
+@Suppress("unused") // See `DefaultInstanceValue` object.
 private val defaultInstances = CacheBuilder.newBuilder()
     .maximumSize(1000.toLong())
     .build(MessageCacheLoader())
@@ -75,10 +100,17 @@ private class MessageCacheLoader : CacheLoader<Class<out Message>, Message>() {
  * Creates a new builder for the passed message class.
  */
 @Internal
+@Suppress("TooGenericExceptionCaught") /*
+  The caught exception is too broad because Kotlin does not have a multi-catch yet:
+     https://discuss.kotlinlang.org/t/does-kotlin-have-multi-catch/486/39
+  We expect `UncheckedExecutionException` or `NoSuchMethodException` if
+  the passed class is just `Class<Message>` or another non-generated `Message` class
+  which does not have a static method `getDefaultInstance()`.
+*/
 public fun <M : Message> builderFor(cls: Class<M>): Message.Builder {
     return try {
         cls.defaultInstance.toBuilder()
-    } catch (e: UncheckedExecutionException) {
+    } catch (e: Exception) {
         throw IllegalArgumentException(
             "Class `${cls.canonicalName}` must be a generated proto message.", e
         )
